e653f97fe52960bffaaf0686c2d270adc8c2226e
Joshua Marx <marx.joshua@gmail.com>
2019-07-26 14:57:05 -0700

    functional prototype

---
+++ src/javascript/server.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const path = require('path');
+const Socrata = require('node-socrata');
+
+const resource = {
+  2016: 'ndkd-k878',
+  2017: 'd4vt-q4t5',
+};
+
+const config = year => {
+  return {
+    hostDomain: 'https://data.lacity.org',
+    resource: resource[year],
+    XAppToken: process.env.SODAPY_APPTOKEN || 'registered-app-token',
+  }
+};
+
+const request = 'Bulky Items';
+
+const params = {
+  $select: ['zipcode', 'createddate', 'requesttype'],
+  $where: `requesttype="${request}"`,
+  $limit: 1000,
+}
+
+const port = 3000;
+
+const app = express();
+const newSoda = year => new Socrata(config(year));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/soda/:year', (req, res) => {
+  const { year } = req.params;
+  const soda = newSoda(year);
+
+  soda.get(params, (err, response, data) => {
+    if (err) console.error(err);
+    else res.send(data);
+  });
+})
+
+app.listen(port, () => { console.log(`Listening on port: ${port}`)});
+
