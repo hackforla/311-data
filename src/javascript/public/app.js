059a8533b4a1a178c0604615361f0417523ac98b
Joshua Marx <marx.joshua@gmail.com>
2019-07-30 11:28:16 -0700

    Func to find DOM elements. Parse data on fetch. Better request handling

--- src/javascript/public/app.js
+++ src/javascript/public/app.js
@@ -1,11 +1,10 @@
 (function(){
   let chartData = [];
-  let countRequestType = [];
 
   const chart = c3.generate({
     bindto: '#chart',
     data: {
-      type: document.querySelector('.chart-type').value,
+      type: getChartTypeDisplay(),
       columns: [],
     },
     axis: {
@@ -15,64 +14,80 @@
       }
   });
 
-  function buildChart() {
-    const requestType = document.querySelector('.request-dropdown').value;
-    const chartType = document.querySelector('.chart-type').value;
-    const datasets = document.querySelectorAll('.data-dropdown');
-
-    Promise.all([
-      getData(datasets[0].value, requestType),
-      getTotal(datasets[0].value, requestType),
-      getData(datasets[1].value, requestType),
-      getTotal(datasets[1].value, requestType),
-    ])
-      .then(() => { renderChart(chartData, countRequestType, chartType); })
-      .catch(err => { console.error('Render Error :-S', err)});
-
-   };
-
-  function renderChart(chartData, countRequestType, type) {
-    const isDisplayTotal = document.querySelector('input[name="total"]').checked;
-    let columns = [];
-
-    if (!isDisplayTotal) {
-      const newData = chartData.reduce((acc, cur) => {
-        const date = cur.createddate.slice(0, 4);
-        if (acc[date]) acc[date].push(parseInt(cur.zipcode));
-        else acc[date] = [];
-        return acc;
-      }, {})
-
-      for (let key in newData) {
-        newData[key].unshift(key);
-        columns.push(newData[key]);
-      }
-    } else {
-      columns = countRequestType;
-    }
-
-    chart.load({ columns, type });
+  function getChartTypeDisplay() {
+    return document.querySelector('.chart-type').value;
   };
+
+  function getDatasetsToDisplay() {
+    return document.querySelectorAll('.data-dropdown');
+  };
+
+  function getRequestType() {
+    return document.querySelector('.request-dropdown').value;
+  };
+
+  function getDisplayTotal() {
+    return document.querySelector('input[name="total"]').checked;
+  }
 
   function getData(year, requestType) {
     return fetch(`/soda/${year}/${requestType}`)
       .then(res => res.json())
-      .then(data => { chartData.push(...data); })
+      .then(data => parseData(data, 'zipcode'))
+      .then(columns => { chartData.push(...columns); })
       .catch(err => { console.error('Fetch Error :-S', err); });
   };
 
   function getTotal(year, requestType) {
     return fetch(`/soda/${year}/${requestType}/total`)
       .then(res => res.json())
-      .then(data => { countRequestType.push([year, parseInt(data[0].count_requesttype)]); })
+      .then(data => parseInt(data[0].count_requesttype))
+      .then(total => { chartData.push([year, total]); })
       .catch(err => { console.error('Fetch Error :-S', err); })
+  };
+
+  function parseData(data, type) {
+    const columns = [];
+    const newData = data.reduce((acc, cur) => {
+        const date = cur.createddate.slice(0, 4);
+        if (acc[date]) acc[date].push(parseInt(cur[type]));
+        else acc[date] = [];
+        return acc;
+      }, {});
+
+    for (let key in newData) {
+      newData[key].unshift(key);
+      columns.push(newData[key]);
+    }
+
+    return columns;
   }
+
+  function buildChart() {
+    const requestType = getRequestType();
+    const chartType = getChartTypeDisplay();
+    const displayTotal = getDisplayTotal();
+    const datasets = [...getDatasetsToDisplay()]
+                      .map(dataset => {
+                        const { value } = dataset;
+                        if (!displayTotal) return getData(value, requestType);
+                        else return getTotal(value, requestType);
+                      });
+
+    Promise.all(datasets)
+      .then(() => { renderChart(chartData, chartType); })
+      .catch(err => { console.error('Render Error :-S', err)});
+
+   };
+
+  function renderChart(columns, type) {
+    chart.load({ columns, type });
+  };
 
   document.querySelector('button').onclick = e => {
     e.preventDefault();
     chart.unload();
     chartData = [];
-    countRequestType = [];
     buildChart();
   };
 
