364f105383902f7c691126f977e67fb93bce67d7
Russell <sell_nat@yahoo.com>
2019-08-01 23:52:44 -0700

    Added boilerplate flask, docker, compose
    Removed spaces and organized imports
    
    Cleaned up backed dockerization
    
    Added boilerplate react app and dockerization
    
    Dockerified front end, connected front and backend, switched out compose-default
    
    Ignoring the compose file
    
    Allowed token entry from config
    
    Modified ignore
    
    Excluded config from ignore
    
    Added default config, removed tracked config
    
    Added readme for orchestration, added 311-user to postgresdb
    
    Added orchestration configuration info in the readmes

---
+++ Project/javascript/react/311-data/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import ApiButtons from './components/ApiButtons.js';
+
+function App() {
+  return (
+    <div className="App">
+      <ApiButtons />
+    </div>
+  );
+}
+
+export default App;
