1c3d4c9302c09e717bf40d4c29d0549e970adbeb
Joshua Marx <j.marx@Joshuas-MacBook-Pro.local>
2019-09-24 20:13:45 -0700

    Updated imports

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
-const express = require('./node_modules/express');
+const express = require('express');
 const path = require('path');
-const Socrata = require('./node_modules/node-socrata');
+const Socrata = require('node-socrata');
 
 const app = express();
 const port = 3000;
