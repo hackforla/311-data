081733cc3d83a98dd789667ec943de1889b2deea
Joshua Marx <j.marx@Joshuas-MacBook-Pro.local>
2019-09-24 21:02:17 -0700

    Converting to react

--- src/components/PinMap/PinMap.jsx
+++ src/components/PinMap/PinMap.jsx
@@ -2,9 +2,32 @@ import React, { Component } from 'react';
 import { Map, Marker, Popup, TileLayer } from 'react-leaflet';
 import { mapAccessToken } from '../../config.js';
 
+const serviceRequests = [
+  'Bulky Items',
+  'Dead Animal Removal',
+  'Electronic Waste',
+  'Graffiti Removal',
+  'Homeless Encampment',
+  'Illegal Dumping Pickup',
+  'Metal/Household Appliances',
+  'Single Streetlight Issue',
+  'Multiple Streetlight Issue',
+  'Report Water Waste',
+  'Other',
+];
+
+const years = [
+  '2015',
+  '2016',
+  '2017',
+  '2018',
+  '2019',
+];
+
 class PinMap extends Component {
   constructor(props) {
     super(props);
+
     this.state = {
       position: [34.0173157, -118.2497254],
       zoom: 10,
@@ -12,19 +35,40 @@ class PinMap extends Component {
     }
   }
 
+  onDropdownChange = () => {
+
+  }
+
   renderMap = () => {
     const { position, zoom, mapUrl } = this.state;
 
     return (
-      <Map center={position} zoom={zoom}>
-        <TileLayer
-          url={mapUrl}
-          attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
-        />
-        <Marker position={position}>
-          <Popup>A pretty CSS3 popup.<br />Easily customizable.</Popup>
-        </Marker>
-      </Map>
+      <div className="pinmap-container">
+        <div className="map-container">
+          <Map center={position} zoom={zoom}>
+            <TileLayer
+              url={mapUrl}
+              attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
+            />
+            <Marker position={position}>
+              <Popup>A pretty CSS3 popup.<br />Easily customizable.</Popup>
+            </Marker>
+          </Map>
+        </div>
+        <div className="dropdown-container">
+          Year
+          &nbsp;
+          <select class="year-dropdown">
+            {years.map(year => (<option value={year}>{year}</option>))}
+          </select>
+          <br/>
+          Service Request
+          &nbsp;
+          <select class="request-dropdown">
+            {serviceRequests.map(service => (<option value={service}>{service}</option>))}
+          </select>
+        </div>
+      </div>
     )
   }
 
