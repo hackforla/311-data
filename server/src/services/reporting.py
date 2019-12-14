38d8fbbed61e9906f0af8bdd38a10a5256ca632f
sellnat77 <sell_nat@yahoo.com>
2019-12-13 17:45:18 -0800

    Created reporting worker that retrieves data from postgres

--- server/src/services/reporting.py
+++ server/src/services/reporting.py
@@ -1,26 +1,30 @@
 import pandas as pd
+import sqlalchemy as db
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.types import Integer, Text, String, DateTime, Float
 from sqlalchemy import create_engine
+import tempfile
 
 class reports(object):
-    def __init__(self):
+    def __init__(self, config=None):
+        self.config = config
+        self.dbString = None if not self.config else self.config['Database']['DB_CONNECTION_STRING']
         pass
 
-    def biggestOffenderCSV(self, dataset, startDate, requestType, councilName):
+    def biggestOffenderCSV(self, startDate="2018-12-01", requestType="Bulky Items", councilName="HISTORIC HIGHLAND PARK NC"):
         '''Output data as CSV by council name, requset type, and
         start date (pulls to current date). Arguments should be passed
         as strings. Date values must be formatted %Y-%m-%d.'''
-        # Query should resemble SELECT * FROM ingest_staging_table WHERE CreatedDate > startDate AND RequestType = requestType AND NCName = councilName
         engine = create_engine(self.dbString)
-        df = pd.read_sql(query.statement, query.session.bind)
-        #df = dataset.copy() # Shard deepcopy to allow multiple endpoints
-        # Data filtering
-        #dateFilter = df['CreatedDate'] > startDate
-        #requestFilter = df['RequestType'] == requestType
-        #councilFilter = df['NCName'] == councilName
-        #df = df[dateFilter & requestFilter & councilFilter]
-        # Return string object for routing to download
+
+        sql = "SELECT * \
+               FROM ingest_staging_table \
+               WHERE CreatedDate > '{}' \
+               AND RequestType = '{}' \
+               AND NCName = '{}'"\
+        .format(startDate, requestType, councilName)
+
+        df = pd.read_sql_query(sql, engine)
         return df.to_csv()
 
 if __name__ == "__main__":
