79ffb1cddcab536c25206483321222f5ef20a13c
Adam Kendis <atkendis@gmail.com>
2020-01-28 14:24:03 -0800

    Bulma-checkradio import.

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import 'bulma';
+import 'bulma-checkradio';
 
 import store from './redux/store';
 import App from './App';
