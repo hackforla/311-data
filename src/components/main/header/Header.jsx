e7949ad99204304cfbedac12901930297765e90f
Joshua <marx.joshua@gmail.com>
2020-02-04 22:10:32 -0800

    Renamed footer and header elements

--- src/components/main/header/Header.jsx
+++ src/components/main/header/Header.jsx
@@ -17,10 +17,8 @@ const Header = () => {
     fontWeight: 'bold',
   };
 
-  // const quickviews = bulmaQuickview.attach();
-
   return (
-    <nav
+    <header
       className="navbar"
       role="navigation"
       aria-label="main navigation"
@@ -56,7 +54,7 @@ const Header = () => {
           </div>
         </div>
       </div>
-    </nav>
+    </header>
   );
 };
 
