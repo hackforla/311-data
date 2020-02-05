ef8af97eb2df67251d10d5ca63a51c052ade97ce
Joshua <marx.joshua@gmail.com>
2020-02-04 22:11:10 -0800

    Sidebar slides in and out with button toggle

--- src/components/main/body/Menu.jsx
+++ src/components/main/body/Menu.jsx
@@ -1,31 +1,66 @@
 import React, { useState } from 'react';
-import Sidebar from 'react-sidebar';
+import { slide as Sidebar } from 'react-burger-menu';
+import Button from '../../common/Button';
 
 // const buildDataUrl = () => {
 //   return `https://data.lacity.org/resource/${dataResources[year]}.json?$select=location,zipcode,address,requesttype,status,ncname,streetname,housenumber&$where=date_extract_m(CreatedDate)+between+${startMonth}+and+${endMonth}+and+requesttype='${request}'`;
 // };
 
 const Menu = () => {
-  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+  const [isOpen, setIsOpen] = useState(true);
+  const sidebarWidth = '500px';
+
   return (
-    <>
+    <div>
       <Sidebar
-        open={isSidebarOpen}
-        sidebar={(
-          <div>
-            THIS IS A TEST
-            <button
-              type="button"
-              className="button is-primary"
-              onClick={() => setIsSidebarOpen(!isSidebarOpen)}
-            >
-              Show quickview
-            </button>
+        htmlClassName="sidebar-html"
+        bodyClassName="sidebar-body"
+        pageWrapId="sidebar-wrapper"
+        outerContainerId="body-container"
+        noOverlay
+        isOpen={isOpen}
+        width={sidebarWidth}
+        customBurgerIcon={false}
+        customCrossIcon={false}
+        styles={{
+          bmMenu: {
+            background: 'white',
+            boxShadow: '0 4px 5px grey',
+          },
+        }}
+      >
+        <div id="sidebar-wrapper">
+          <Button
+            id="menu-toggle-button"
+            label="<"
+            style={{
+              position: 'fixed',
+              left: sidebarWidth,
+              height: '60px',
+              width: '26px',
+              boxShadow: '0 1px 2px grey',
+            }}
+            handleClick={() => setIsOpen(!isOpen)}
+            color="light"
+          />
+          <div
+            className="tabs is-fullwidth is-toggle"
+            style={{
+              height: '40px',
+            }}
+          >
+            <ul>
+              <li className="is-active" style={{ background: 'yellow', width: '254px' }}>
+                <a>Map</a>
+              </li>
+              <li style={{ width: '254px' }}>
+                <a>Data Visualization</a>
+              </li>
+            </ul>
           </div>
-        )}
-        styles={{ sidebar: { background: 'white' } }}
-      />
-    </>
+        </div>
+      </Sidebar>
+    </div>
   );
 };
 
