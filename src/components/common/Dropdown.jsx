e5c7ff1b375d514764d0e10bee15cebb497bea73
Adam Kendis <atkendis@gmail.com>
2020-02-07 10:49:52 -0800

    Dropdown component in progress.

--- src/components/common/Dropdown.jsx
+++ src/components/common/Dropdown.jsx
@@ -1,36 +1,107 @@
-import React from 'react';
+import React, { useState } from 'react';
+import PropTypes from 'proptypes';
+import classNames from 'classnames';
+import Checkbox from './Checkbox';
 
-const Dropdown = () => (
-  <div className="dropdown is-active">
-    <div className="dropdown-trigger">
-      <button className="button" aria-haspopup="true" aria-controls="dropdown-menu">
-        <span>Dropdown button</span>
-        <span className="icon is-small">
-          <i className="fas fa-angle-down" aria-hidden="true"></i>
-        </span>
-      </button>
-    </div>
-    <div className="dropdown-menu" id="dropdown-menu" role="menu">
-      <div className="dropdown-content">
-        <a href="#" className="dropdown-item">
-          Dropdown item
-        </a>
-        <a className="dropdown-item">
-          Other dropdown item
-        </a>
-        <a href="#" className="dropdown-item is-active">
-          Active dropdown item
-        </a>
-        <a href="#" className="dropdown-item">
-          Other dropdown item
-        </a>
-        <hr className="dropdown-divider" />
-        <a href="#" className="dropdown-item">
-          With a divider
-        </a>
+const items = ['Last Week', 'Last Month', 'Last 6 Months', 'Last 12 Months', 'Year to Date', 'Custom Range'];
+
+const DropdownItem = ({
+  label,
+
+  tag,
+  active,
+  hoverHandler
+
+  // handleOptionClick,
+}) => {
+  const [isActive, toggleItemActive] = useState(!!active);
+  const dropdownItemClassName = classNames('dropdown-item', {
+    'is-active': isActive,
+  });
+
+  return (
+    <a
+      href="# "
+      className={dropdownItemClassName}
+      style={{ paddingRight: '0rem', width: '300px' }}
+      tag={tag}
+      // onMouseEnter={() => toggleItemActive((prevItemActive) => !prevItemActive)}
+      // onMouseLeave={() => toggleItemActive((prevItemActive) => !prevItemActive)}
+    >
+      {label}
+      <span style={{ float: 'right' }}>
+        <Checkbox
+          id={label}
+          circle
+          color="black"
+        />
+      </span>
+    </a>
+  );
+};
+
+
+const Dropdown = ({
+  id,
+  list,
+  label,
+  handleClick,
+  style,
+
+  open,
+  hoverable,
+  rightAligned,
+  dropUp,
+}) => {
+  const [isOpen, toggleOpen] = useState(!!open);
+  const dropdownClassName = classNames('dropdown', {
+    'is-active': isOpen,
+    'is-hoverable': hoverable,
+    'is-right': rightAligned,
+    'is-up': dropUp,
+  });
+
+  return (
+    <div className={dropdownClassName} style={style}>
+      <div className="dropdown-trigger" style={{ width: '100%'}}>
+        <button
+          type="button"
+          className="button"
+          onClick={() => toggleOpen((prevActive) => !prevActive)}
+          style={{ display: 'block', width: '100%' }}
+        >
+          <span style={{ float: 'left' }}>{label}</span>
+          <span style={{ float: 'right' }}>&#8964;</span>
+        </button>
+      </div>
+      <div className="dropdown-menu" id="dropdown-menu" role="menu">
+        <div className="dropdown-content">
+
+          {
+            list.map((item) => {
+              return (
+                <DropdownItem
+                  key={item}
+                  label={item}
+                />
+              );
+            })
+          }
+
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Dropdown;
+
+
+Dropdown.propTypes = {
+
+
+};
+
+Dropdown.defaultProps = {
+
+};
