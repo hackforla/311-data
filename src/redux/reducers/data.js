a1a204c1d64dbae044386c2d4746a92892980e34
Joshua <marx.joshua@gmail.com>
2020-02-12 11:45:25 -0800

    testing checkbox functionality

--- src/redux/reducers/data.js
+++ src/redux/reducers/data.js
@@ -1,11 +1,13 @@
 // import axios from 'axios';
 
+import { COUNCILS } from '../../components/common/CONSTANTS';
+
 const types = {
   UPDATE_YEAR: 'UPDATE_YEAR',
   UPDATE_START_MONTH: 'UPDATE_START_MONTH',
   UPDATE_END_MONTH: 'UPDATE_END_MONTH',
   UPDATE_REQUEST_TYPE: 'UPDATE_REQUEST_TYPE',
-  UPDATE_NEIGHBORHOOD_COUNCIL: 'UPDATE_NEIGHBORHOOD_COUNCIL',
+  TOGGLE_NEIGHBORHOOD_COUNCIL: 'TOGGLE_NEIGHBORHOOD_COUNCIL',
 };
 
 export const updateYear = (year) => ({
@@ -28,17 +30,22 @@ export const updateRequestType = (requestType) => ({
   payload: requestType,
 });
 
-export const updateNeighborhoodCouncil = (council) => ({
-  type: types.UPDATE_NEIGHBORHOOD_COUNCIL,
+export const toggleNeighborhoodCouncil = (council) => ({
+  type: types.TOGGLE_NEIGHBORHOOD_COUNCIL,
   payload: council,
 });
+
+const makeCouncilState = () => COUNCILS.reduce((acc, council) => {
+  acc[council] = false;
+  return acc;
+}, { all: false });
 
 const initialState = {
   year: '2015',
   startMonth: '1',
   endMonth: '12',
   requestType: 'Bulky Items',
-  council: null,
+  councils: makeCouncilState(),
 };
 
 export default (state = initialState, action) => {
@@ -63,11 +70,18 @@ export default (state = initialState, action) => {
         ...state,
         requestType: action.payload,
       };
-    case types.UPDATE_NEIGHBORHOOD_COUNCIL:
+    case types.TOGGLE_NEIGHBORHOOD_COUNCIL: {
+      const { payload } = action;
+
+      const newCouncilsState = { ...state.councils };
+
+      newCouncilsState[payload] = !newCouncilsState[payload];
+
       return {
         ...state,
-        council: action.payload,
+        councils: newCouncilsState,
       };
+    }
     default:
       return state;
   }
