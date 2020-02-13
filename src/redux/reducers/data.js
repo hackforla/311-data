d5fced246fed8f417a230b15aab6447a1d2abee2
Adam Kendis <atkendis@gmail.com>
2020-02-13 12:21:15 -0800

    Replaced redux date-related action types and updated reducer.

--- src/redux/reducers/data.js
+++ src/redux/reducers/data.js
@@ -1,26 +1,38 @@
 import axios from 'axios';
 
 const types = {
-  UPDATE_YEAR: 'UPDATE_YEAR',
-  UPDATE_START_MONTH: 'UPDATE_START_MONTH',
-  UPDATE_END_MONTH: 'UPDATE_END_MONTH',
+  UPDATE_START_DATE: 'UPDATE_START_DATE',
+  UPDATE_END_DATE: 'UPDATE_END_DATE',
+  // UPDATE_YEAR: 'UPDATE_YEAR',
+  // UPDATE_START_MONTH: 'UPDATE_START_MONTH',
+  // UPDATE_END_MONTH: 'UPDATE_END_MONTH',
   UPDATE_REQUEST_TYPE: 'UPDATE_REQUEST_TYPE',
   UPDATE_NEIGHBORHOOD_COUNCIL: 'UPDATE_NEIGHBORHOOD_COUNCIL',
 };
 
-export const updateYear = (year) => ({
-  type: types.UPDATE_YEAR,
-  payload: year,
+// export const updateYear = (year) => ({
+//   type: types.UPDATE_YEAR,
+//   payload: year,
+// });
+
+// export const updateStartMonth = (startMonth) => ({
+//   type: types.UPDATE_START_MONTH,
+//   payload: startMonth,
+// });
+
+// export const updateEndMonth = (endMonth) => ({
+//   type: types.UPDATE_END_MONTH,
+//   payload: endMonth,
+// });
+
+export const updateStartDate = (startDate) => ({
+  type: types.UPDATE_START_DATE,
+  payload: startDate,
 });
 
-export const updateStartMonth = (startMonth) => ({
-  type: types.UPDATE_START_MONTH,
-  payload: startMonth,
-});
-
-export const updateEndMonth = (endMonth) => ({
-  type: types.UPDATE_END_MONTH,
-  payload: endMonth,
+export const updateEndDate = (endDate) => ({
+  type: types.UPDATE_END_DATE,
+  payload: endDate,
 });
 
 export const updateRequestType = (requestType) => ({
@@ -34,29 +46,41 @@ export const updateNeighborhoodCouncil = (council) => ({
 });
 
 const initialState = {
-  year: '2015',
-  startMonth: '1',
-  endMonth: '12',
+  startDate: '01/01/2015',
+  endDate: '02/10/2020',
+  // year: '2015',
+  // startMonth: '1',
+  // endMonth: '12',
   requestType: 'Bulky Items',
   council: null,
 };
 
 export default (state = initialState, action) => {
   switch (action.type) {
-    case types.UPDATE_YEAR:
+    // case types.UPDATE_YEAR:
+    //   return {
+    //     ...state,
+    //     year: action.payload,
+    //   };
+    // case types.UPDATE_START_MONTH:
+    //   return {
+    //     ...state,
+    //     startMonth: action.payload,
+    //   };
+    // case types.UPDATE_END_MONTH:
+    //   return {
+    //     ...state,
+    //     endMonth: action.payload,
+    //   };
+    case types.UPDATE_START_DATE:
       return {
         ...state,
-        year: action.payload,
+        startDate: action.payload,
       };
-    case types.UPDATE_START_MONTH:
+    case types.UPDATE_END_DATE:
       return {
         ...state,
-        startMonth: action.payload,
-      };
-    case types.UPDATE_END_MONTH:
-      return {
-        ...state,
-        endMonth: action.payload,
+        endDate: action.payload,
       };
     case types.UPDATE_REQUEST_TYPE:
       return {
