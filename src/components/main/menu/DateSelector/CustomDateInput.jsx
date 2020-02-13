0dcccd458edfc58d1ded840eddf3b156611c38b4
Adam Kendis <atkendis@gmail.com>
2020-02-13 00:48:49 -0800

    Moved CustomDateInput and DateRangePicker to menu/DateSelector.

---
+++ src/components/main/menu/DateSelector/CustomDateInput.jsx
@@ -0,0 +1,73 @@
+import React, { forwardRef } from 'react';
+import PropTypes from 'proptypes';
+import classNames from 'classnames';
+
+const CustomDateInput = ({
+  value,
+  onClick,
+  onChange,
+  color,
+  size,
+  rounded,
+  hovered,
+  focused,
+  loading,
+  disabled,
+  readOnly,
+  isStatic,
+}, ref) => {
+  const inputClassName = classNames('input', {
+    [`is-${color}`]: color,
+    [`is-${size}`]: size,
+    'is-rounded': rounded,
+    'is-hovered': hovered,
+    'is-focused': focused,
+    'is-loading': loading,
+    'is-static': isStatic,
+  });
+
+  return (
+    <>
+      <input
+        ref={ref}
+        className={inputClassName}
+        value={value}
+        onClick={onClick}
+        onChange={onChange}
+        disabled={disabled}
+        readOnly={readOnly}
+        type="text"
+      />
+    </>
+  );
+};
+
+export default forwardRef(CustomDateInput);
+
+CustomDateInput.propTypes = {
+  value: PropTypes.string,
+  onClick: PropTypes.func.isRequired,
+  onChange: PropTypes.func.isRequired,
+  color: PropTypes.oneOf(['primary', 'info', 'success', 'warning', 'danger']),
+  size: PropTypes.oneOf([undefined, 'small', 'medium', 'large']),
+  rounded: PropTypes.bool,
+  hovered: PropTypes.bool,
+  focused: PropTypes.bool,
+  loading: PropTypes.bool,
+  disabled: PropTypes.bool,
+  readOnly: PropTypes.bool,
+  isStatic: PropTypes.bool,
+};
+
+CustomDateInput.defaultProps = {
+  value: '',
+  color: 'primary',
+  size: undefined,
+  rounded: false,
+  hovered: false,
+  focused: false,
+  loading: false,
+  disabled: false,
+  readOnly: false,
+  isStatic: false,
+};
---
+++ src/components/main/menu/DateSelector/DateRangePicker.jsx
@@ -0,0 +1,187 @@
+import React, { useState } from 'react';
+import PropTypes from 'proptypes';
+import DatePicker from 'react-datepicker';
+import Button from '../../../common/Button';
+
+import 'react-datepicker/dist/react-datepicker.css';
+import COLORS from '../../../../styles/COLORS';
+
+const cardStyle = {
+  height: '225px',
+  width: '350px',
+  overflow: 'visible',
+};
+
+const headerStyle = {
+  height: '50px',
+  background: COLORS.BACKGROUND,
+  color: COLORS.FONTS,
+  fontWeight: 'bold',
+  fontSize: '20px',
+  border: 'none',
+};
+
+const textStyle = {
+  fontSize: '16px',
+};
+
+const inputSpanStyle = {
+  display: 'inline-block',
+  width: '100px',
+  color: headerStyle.color,
+  ...textStyle,
+};
+
+const DateRangePicker = ({
+  id,
+  title,
+  style,
+}) => {
+  const [startDate, updateStartDate] = useState();
+  const [endDate, updateEndDate] = useState();
+
+  const handleDateChange = (updateStartOrEndDate, date) => {
+    updateStartOrEndDate(date);
+  };
+
+  return (
+    <div
+      id={id}
+      className="modal-card"
+      style={{ ...cardStyle, ...style }}
+    >
+      {/* ---------- Modal Card Header ---------- */}
+      <header
+        className="modal-card-head"
+        style={headerStyle}
+      >
+        <p
+          className="modal-card-title has-text-weight-bold is-size-5"
+          style={{ color: headerStyle.color }}
+        >
+          {title}
+        </p>
+        <button
+          type="button"
+          className="delete"
+          aria-label="close"
+          onClick={() => {
+            /*
+             * Dispatch action to close modal
+             */
+          }}
+        />
+      </header>
+
+      {/* ---------- Modal Card Body - main content ---------- */}
+      <section
+        className="modal-card-body"
+        style={{
+          overflow: 'visible',
+          display: 'flex',
+          alignItems: 'center',
+        }}
+      >
+        <div className="container">
+          <div
+            className="container"
+            style={{
+              height: '35px',
+              display: 'flex',
+              alignItems: 'center',
+            }}
+          >
+            &nbsp;
+            <span style={inputSpanStyle}>
+              Start Date
+            </span>
+            <DatePicker
+              todayButton="Today"
+              selected={startDate}
+              startDate={startDate}
+              endDate={endDate}
+              minDate={new Date('2015/1/1')}
+              maxDate={new Date()}
+              selectsStart
+              showYearDropdown
+              showMonthDropdown
+              showPopperArrow={false}
+              popperPlacement="right"
+              onChange={(date) => handleDateChange(updateStartDate, date)}
+              placeholderText="MM/DD/YYYY"
+            />
+          </div>
+          <div
+            className="container"
+            style={{
+              background: COLORS.FORMS.STROKE,
+              height: '35px',
+              display: 'flex',
+              alignItems: 'center',
+            }}
+          >
+            &nbsp;
+            <span style={{ ...inputSpanStyle }}>
+              End Date
+            </span>
+            <DatePicker
+              todayButton="Today"
+              selected={endDate}
+              startDate={startDate}
+              endDate={endDate}
+              minDate={startDate}
+              maxDate={new Date()}
+              showYearDropdown
+              showMonthDropdown
+              selectsStart
+              showPopperArrow={false}
+              popperPlacement="right"
+              onChange={(date) => handleDateChange(updateEndDate, date)}
+              placeholderText="MM/DD/YYYY"
+            />
+          </div>
+        </div>
+      </section>
+
+      {/* ---------- Modal Card Footer - button(s) ---------- */}
+      <footer
+        className="modal-card-foot"
+        style={{
+          background: headerStyle.background,
+          border: 'none',
+          textAlign: 'center',
+        }}
+      >
+        <div className="container">
+          <Button
+            id="date-range-button"
+            label="Save Changes"
+            style={{
+              background: COLORS.BRAND.CTA1,
+              color: COLORS.FONTS,
+              fontWeight: 'bold',
+            }}
+            handleClick={() => {
+              /*
+               * Dispatch start and end dates
+               */
+            }}
+          />
+        </div>
+      </footer>
+    </div>
+  );
+};
+
+export default DateRangePicker;
+
+DateRangePicker.propTypes = {
+  id: PropTypes.string.isRequired,
+  title: PropTypes.string,
+  style: PropTypes.shape({}),
+};
+
+DateRangePicker.defaultProps = {
+  title: undefined,
+  style: undefined,
+};
