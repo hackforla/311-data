8dba2cc6937ec37ecc3326eba8c808ecc196cc7f
Adam Kendis <atkendis@gmail.com>
2020-02-13 14:38:20 -0800

    Date-related redux action creators and reducers.

--- src/redux/reducers/data.js
+++ src/redux/reducers/data.js
@@ -3,11 +3,13 @@ import axios from 'axios';
 const types = {
   UPDATE_START_DATE: 'UPDATE_START_DATE',
   UPDATE_END_DATE: 'UPDATE_END_DATE',
+  UPDATE_REQUEST_TYPE: 'UPDATE_REQUEST_TYPE',
+  UPDATE_NEIGHBORHOOD_COUNCIL: 'UPDATE_NEIGHBORHOOD_COUNCIL',
+  OPEN_DATE_RANGE_MODAL: 'OPEN_DATE_RANGE_MODAL',
+  CLOSE_DATE_RANGE_MODAL: 'CLOSE_DATE_RANGE_MODAL',
   // UPDATE_YEAR: 'UPDATE_YEAR',
   // UPDATE_START_MONTH: 'UPDATE_START_MONTH',
   // UPDATE_END_MONTH: 'UPDATE_END_MONTH',
-  UPDATE_REQUEST_TYPE: 'UPDATE_REQUEST_TYPE',
-  UPDATE_NEIGHBORHOOD_COUNCIL: 'UPDATE_NEIGHBORHOOD_COUNCIL',
 };
 
 // export const updateYear = (year) => ({
@@ -25,14 +27,22 @@ const types = {
 //   payload: endMonth,
 // });
 
-export const updateStartDate = (startDate) => ({
-  type: types.UPDATE_START_DATE,
-  payload: startDate,
+export const openDateRangeModal = () => ({
+  type: types.OPEN_DATE_RANGE_MODAL,
 });
 
-export const updateEndDate = (endDate) => ({
+export const closeDateRangeModal = () => ({
+  type: types.CLOSE_DATE_RANGE_MODAL,
+});
+
+export const updateStartDate = (newStartDate) => ({
+  type: types.UPDATE_START_DATE,
+  payload: newStartDate,
+});
+
+export const updateEndDate = (newEndDate) => ({
   type: types.UPDATE_END_DATE,
-  payload: endDate,
+  payload: newEndDate,
 });
 
 export const updateRequestType = (requestType) => ({
@@ -48,40 +58,39 @@ export const updateNeighborhoodCouncil = (council) => ({
 const initialState = {
   startDate: '01/01/2015',
   endDate: '02/10/2020',
+  requestType: 'Bulky Items',
+  council: null,
   // year: '2015',
   // startMonth: '1',
   // endMonth: '12',
-  requestType: 'Bulky Items',
-  council: null,
 };
 
 export default (state = initialState, action) => {
   switch (action.type) {
-    // case types.UPDATE_YEAR:
-    //   return {
-    //     ...state,
-    //     year: action.payload,
-    //   };
-    // case types.UPDATE_START_MONTH:
-    //   return {
-    //     ...state,
-    //     startMonth: action.payload,
-    //   };
-    // case types.UPDATE_END_MONTH:
-    //   return {
-    //     ...state,
-    //     endMonth: action.payload,
-    //   };
-    case types.UPDATE_START_DATE:
+    case types.OPEN_DATE_RANGE_MODAL: {
+      return {
+        ...state,
+        dateRangeModalOpen: true,
+      };
+    }
+    case types.CLOSE_DATE_RANGE_MODAL: {
+      return {
+        ...state,
+        dateRangeModalOpen: false,
+      };
+    }
+    case types.UPDATE_START_DATE: {
       return {
         ...state,
         startDate: action.payload,
       };
-    case types.UPDATE_END_DATE:
+    }
+    case types.UPDATE_END_DATE: {
       return {
         ...state,
         endDate: action.payload,
       };
+    }
     case types.UPDATE_REQUEST_TYPE:
       return {
         ...state,
