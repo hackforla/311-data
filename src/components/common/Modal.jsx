7a8d3f24583224aff4f271237d59da8c43dc55de
Adam Kendis <atkendis@gmail.com>
2020-02-15 21:12:12 -0800

    Redux removed from modal. Modal open state is now controlled by parent component.

--- src/components/common/Modal.jsx
+++ src/components/common/Modal.jsx
@@ -1,36 +1,15 @@
-import React, { useEffect } from 'react';
-import { connect } from 'react-redux';
+import React from 'react';
 import PropTypes from 'proptypes';
 import classNames from 'classnames';
-import { closeDateRangeModal } from '../../redux/reducers/data';
 
 const Modal = ({
   open,
   content,
   style,
-  closeModal,
 }) => {
   const modalClassName = classNames('modal', {
     'is-active': open,
   });
-
-  useEffect(() => {
-    const handleEscapeClick = (e) => {
-      if (e.keyCode === 27) {
-        closeModal();
-      }
-    };
-
-    if (open) {
-      document.addEventListener('keydown', handleEscapeClick);
-    } else {
-      document.removeEventListener('keydown', handleEscapeClick);
-    }
-
-    return () => {
-      document.removeEventListener('keydown', handleEscapeClick);
-    };
-  }, [open, closeModal]);
 
   return (
     <div
@@ -48,24 +27,12 @@ const Modal = ({
   );
 };
 
-const mapStateToProps = (state) => ({
-  open: state.data.dateRangeModalOpen,
-});
-
-const mapDispatchToProps = (dispatch) => ({
-  closeModal: () => dispatch(closeDateRangeModal()),
-});
-
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps,
-)(Modal);
+export default Modal;
 
 Modal.propTypes = {
   open: PropTypes.bool,
   content: PropTypes.element.isRequired,
   style: PropTypes.shape({}),
-  closeModal: PropTypes.func.isRequired,
 };
 
 Modal.defaultProps = {
