3c2d2518dbbefec3eb6515bec3b7ca75035efc87
Joshua Marx <marx.joshua@gmail.com>
2020-02-17 10:27:11 -0800

    Disabled jsx-a11y for now

--- src/components/main/menu/Menu.jsx
+++ src/components/main/menu/Menu.jsx
@@ -1,3 +1,6 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
 import React, { useState } from 'react';
 import { slide as Sidebar } from 'react-burger-menu';
 
