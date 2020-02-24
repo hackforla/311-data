ce088d4e79d24eb74e416880aadf83d9e9b262a7
Jake Mensch <jake.mensch@gmail.com>
2020-02-24 14:16:08 -0800

    activated HMR and redux devtools

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,6 @@
 import { applyMiddleware, createStore } from 'redux';
 import logger from 'redux-logger';
+import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly';
 
 import rootReducer from './rootReducer';
 
@@ -9,9 +10,8 @@ if (process.env.NODE_ENV === 'development') {
   middlewares.push(logger);
 }
 
-const store = createStore(
-  rootReducer,
+const store = createStore(rootReducer, composeWithDevTools(
   applyMiddleware(...middlewares),
-);
+));
 
 export default store;
