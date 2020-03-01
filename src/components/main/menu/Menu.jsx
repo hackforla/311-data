dc0a0641762683b473babd8e6f75ba624c28ae12
Joshua <marx.joshua@gmail.com>
2020-03-01 11:58:53 -0800

    Removed unused comments

--- src/components/main/menu/Menu.jsx
+++ src/components/main/menu/Menu.jsx
@@ -18,11 +18,6 @@ import DateSelector from './DateSelector/DateSelector';
 import NCSelector from './NCSelector';
 import RequestTypeSelector from './RequestTypeSelector';
 
-
-// const buildDataUrl = () => {
-//   return `https://data.lacity.org/resource/${dataResources[year]}.json?$select=location,zipcode,address,requesttype,status,ncname,streetname,housenumber&$where=date_extract_m(CreatedDate)+between+${startMonth}+and+${endMonth}+and+requesttype='${request}'`;
-// };
-
 const Menu = ({
   isOpen,
   activeTab,
