ca449470958f7830ce329f9ab54a868440c62b3f
Adam Kendis <atkendis@gmail.com>
2020-03-26 16:23:10 -0700

    About 311 page components: About, HeroImage, HowItWorks, WhatIs311Data.

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,8 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import Button from '@components/common/Button';
+import HeroImage from './HeroImage';
+import WhatIs311Data from './WhatIs311Data';
+import HowItWorks from './HowItWorks';
 
 const About = () => (
-  <div>
-    About
+  <div className="about-311">
+    <HeroImage />
+    <WhatIs311Data />
+    <HowItWorks />
+    <Link to="/">
+      <Button id="about-311" label="Let's Get Started" />
+    </Link>
   </div>
 );
 
