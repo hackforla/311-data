368cb4c632a1cd9fdc9e1833da1f9f8fd6c172eb
Adam Kendis <atkendis@gmail.com>
2020-04-02 15:49:43 -0700

    Updated filter and comparisonFilters reducers for request types and date selection.

--- src/redux/reducers/comparisonFilters.js
+++ src/redux/reducers/comparisonFilters.js
@@ -1,38 +1,38 @@
 import { REQUEST_TYPES } from '@components/common/CONSTANTS';
 
 export const types = {
-  UPDATE_START_DATE: 'UPDATE_START_DATE',
-  UPDATE_END_DATE: 'UPDATE_END_DATE',
-  UPDATE_REQUEST_TYPE: 'UPDATE_REQUEST_TYPE',
+  UPDATE_COMPARISON_START_DATE: 'UPDATE_COMPARISON_START_DATE',
+  UPDATE_COMPARISON_END_DATE: 'UPDATE_COMPARISON_END_DATE',
+  UPDATE_COMPARISON_REQUEST_TYPE: 'UPDATE_COMPARISON_REQUEST_TYPE',
   UPDATE_NEIGHBORHOOD_COUNCIL: 'UPDATE_NEIGHBORHOOD_COUNCIL',
   UPDATE_COMPARISON_LIST: 'UPDATE_COMPARISON_LIST',
   UPDATE_COMPARISON_DISTRICT: 'UPDATE_COMPARISON_DISTRICT',
   UPDATE_COMPARISON_CHART: 'UPDATE_COMPARISON_CHART',
-  SELECT_ALL_REQUEST_TYPES: 'SELECT_ALL_REQUEST_TYPES',
-  DESELECT_ALL_REQUEST_TYPES: 'DESELECT_ALL_REQUEST_TYPES',
+  SELECT_ALL_COMPARISON_REQUEST_TYPES: 'SELECT_ALL_COMPARISON_REQUEST_TYPES',
+  DESELECT_ALL_COMPARISON_REQUEST_TYPES: 'DESELECT_ALL_COMPARISON_REQUEST_TYPES',
 };
 
-export const updateStartDate = newStartDate => ({
-  type: types.UPDATE_START_DATE,
+export const updateComparisonStartDate = newStartDate => ({
+  type: types.UPDATE_COMPARISON_START_DATE,
   payload: newStartDate,
 });
 
-export const updateEndDate = newEndDate => ({
-  type: types.UPDATE_END_DATE,
+export const updateComparisonEndDate = newEndDate => ({
+  type: types.UPDATE_COMPARISON_END_DATE,
   payload: newEndDate,
 });
 
-export const updateRequestType = requestTypes => ({
-  type: types.UPDATE_REQUEST_TYPE,
+export const updateComparisonRequestType = requestTypes => ({
+  type: types.UPDATE_COMPARISON_REQUEST_TYPE,
   payload: requestTypes,
 });
 
-export const selectAllRequestTypes = () => ({
-  type: types.SELECT_ALL_REQUEST_TYPES,
+export const selectAllComparisonRequestTypes = () => ({
+  type: types.SELECT_ALL_COMPARISON_REQUEST_TYPES,
 });
 
-export const deselectAllRequestTypes = () => ({
-  type: types.DESELECT_ALL_REQUEST_TYPES,
+export const deselectAllComparisonRequestTypes = () => ({
+  type: types.DESELECT_ALL_COMPARISON_REQUEST_TYPES,
 });
 
 export const updateNC = council => ({
@@ -89,33 +89,33 @@ const initialState = {
 
 export default (state = initialState, action) => {
   switch (action.type) {
-    case types.UPDATE_START_DATE: {
+    case types.UPDATE_COMPARISON_START_DATE: {
       return {
         ...state,
         startDate: action.payload,
       };
     }
-    case types.UPDATE_END_DATE: {
+    case types.UPDATE_COMPARISON_END_DATE: {
       return {
         ...state,
         endDate: action.payload,
       };
     }
-    case types.UPDATE_REQUEST_TYPE:
+    case types.UPDATE_COMPARISON_REQUEST_TYPE:
       return {
         ...state,
         requestTypes: {
           ...state.requestTypes,
-          // Flips boolean value for selected request type
+          All: false,
           [action.payload]: !state.requestTypes[action.payload],
         },
       };
-    case types.SELECT_ALL_REQUEST_TYPES:
+    case types.SELECT_ALL_COMPARISON_REQUEST_TYPES:
       return {
         ...state,
         requestTypes: allRequestTypes(true),
       };
-    case types.DESELECT_ALL_REQUEST_TYPES:
+    case types.DESELECT_ALL_COMPARISON_REQUEST_TYPES:
       return {
         ...state,
         requestTypes: initialState.requestTypes,
