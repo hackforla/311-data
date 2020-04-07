6391e98afee8e656dcddceb09f4e76f62b81c220
Jake Mensch <jake.mensch@gmail.com>
2020-04-06 18:03:17 -0700

    organized scss files

--- src/components/main/footer/Footer.jsx
+++ src/components/main/footer/Footer.jsx
@@ -5,39 +5,20 @@ import propTypes from 'proptypes';
 import moment from 'moment';
 import StaticFooter from './StaticFooter';
 
-import COLORS from '../../../styles/COLORS';
-
-const footerTextStyle = {
-  color: COLORS.BACKGROUND,
-  fontWeight: 'bold',
-};
-
 const Footer = ({
   lastUpdated,
 }) => (
-  <footer
-    className="navbar has-navbar-fixed-bottom"
-    style={{
-      position: 'fixed',
-      bottom: '0',
-      height: '45px',
-      background: '#002449',
-    }}
-  >
-    <div className="level" style={{ width: '100vw' }}>
-      <Switch>
-        <Route path="/(about|contact)" component={StaticFooter} />
-        <Route path="/">
-          <div className="level-item">
-            <p style={footerTextStyle}>
-              Data Updated Through:
-              &nbsp;
-              {lastUpdated && moment(1000 * lastUpdated).format('MMMM Do YYYY, h:mm:ss a')}
-            </p>
-          </div>
-        </Route>
-      </Switch>
-    </div>
+  <footer className="navbar has-navbar-fixed-bottom">
+    <Switch>
+      <Route path="/(about|contact)" component={StaticFooter} />
+      <Route path="/">
+        <p>
+          Data Updated Through:
+          &nbsp;
+          {lastUpdated && moment(1000 * lastUpdated).format('MMMM Do YYYY, h:mm:ss a')}
+        </p>
+      </Route>
+    </Switch>
   </footer>
 );
 
