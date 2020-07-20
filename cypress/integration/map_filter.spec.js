ec886950b43a2d6668e54e9f561038971316a84f
croor005 <richard.crook@disney.com>
2020-07-20 16:34:41 -0700

    Adding more JSON response tests

--- cypress/integration/map_filter.spec.js
+++ cypress/integration/map_filter.spec.js
@@ -1,4 +1,4 @@
-describe('Filter Selection', () => {
+describe('Filters Selection', () => {
 
     it('Map: Last Week, Dead Animal [DAN]', () => {
 
@@ -23,12 +23,21 @@ describe('Filter Selection', () => {
         cy.wait('@getFilterList').should((xhr) => {
             const myCount = xhr.responseBody[0].count
             cy.get('#root > div.body.menu-is-open > div.map-container > div > div.leaflet-pane.leaflet-map-pane > div.leaflet-pane.leaflet-marker-pane > div > div > span').should('have.text', myCount)
-            cy.get('#icon-request-icon-DAN').should("exist")
         })
 
     });
 
-    it.skip('Data Visualization: Last Week, Dead Animal [DAN]', () => {
+    it('Data Visualization: Last Week, Dead Animal [DAN]', () => {
+
+        cy.server()
+        cy.route({
+            method: 'POST',
+            url: '/map/clusters',
+            onResponse(xhr) {
+                expect(xhr.status).to.eq(200)
+            }
+        }).as('getFilterList')
+
         cy.visit('/')
         cy.get('#root > header > div.navbar-brand > a').click()
         cy.get('#root > div > div.menu-container > div.menu-tabs > a:nth-child(2)').click()
@@ -37,7 +46,12 @@ describe('Filter Selection', () => {
         cy.get(':nth-child(2) > .select-group-content > .select-item > .select-item-box').click()
         cy.get('#type-selector-container > div.columns.is-0 > div:nth-child(1) > div:nth-child(2) > span > input').click({force: true})
         cy.get('#btn-sidebar-submit-button').click()
-        cy.get('#icon-request-icon-DAN').should("exist");
+
+        cy.wait('@getFilterList').should((xhr) => {
+            const myCount = xhr.responseBody[0].count
+            cy.get('.requests-box').should('have.text', myCount)
+        })
+
     });
 
 })
