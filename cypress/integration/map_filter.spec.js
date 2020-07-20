b7dbe12afb96e62616c1ad451927e361cb557aa9
croor005 <richard.crook@disney.com>
2020-07-20 10:19:59 -0700

    Adding Map and Data Visualization tests Setting up global config file

--- cypress/integration/map_filter.spec.js
+++ cypress/integration/map_filter.spec.js
@@ -1,14 +1,29 @@
 
 describe('Filter Selection', () => {
-  it('Select some filters: Last Week, Dead Animal [DAN]', () => {
-    cy.visit('http://localhost:3000')
+
+  it('Map: Last Week, Dead Animal [DAN]', () => {
+    cy.visit('/')
     cy.get('#root > header > div.navbar-brand > a').click()
+    cy.get('#root > div > div.menu-container > div.menu-tabs > a:nth-child(1)').click()
     cy.get('.dropdown-trigger > .button').click()
     cy.get('[value="LAST_WEEK"]').click()
     cy.get(':nth-child(2) > .select-group-content > .select-item > .select-item-box').click()
-    cy.get('#type-selector-container > div.columns.is-0 > div:nth-child(1) > div:nth-child(2) > span > input').click()
+    cy.get('#type-selector-container > div.columns.is-0 > div:nth-child(1) > div:nth-child(2) > span > input').click({force: true})
     cy.get('#btn-sidebar-submit-button').click()
     cy.get('#icon-request-icon-DAN').should("exist");
   });
+
+  it('Data Visualization: Last Week, Dead Animal [DAN]', () => {
+    cy.visit('/')
+    cy.get('#root > header > div.navbar-brand > a').click()
+    cy.get('#root > div > div.menu-container > div.menu-tabs > a:nth-child(2)').click()
+    cy.get('.dropdown-trigger > .button').click()
+    cy.get('[value="LAST_WEEK"]').click()
+    cy.get(':nth-child(2) > .select-group-content > .select-item > .select-item-box').click()
+    cy.get('#type-selector-container > div.columns.is-0 > div:nth-child(1) > div:nth-child(2) > span > input').click({force: true})
+    cy.get('#btn-sidebar-submit-button').click()
+    cy.get('#icon-request-icon-DAN').should("exist");
+  });
+
 })
 
