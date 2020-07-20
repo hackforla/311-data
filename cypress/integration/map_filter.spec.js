e1444f979f2e50829c54a97cd0be22e818d8319b
croor005 <richard.crook@disney.com>
2020-07-20 16:04:35 -0700

    Adding JSON response test

--- cypress/integration/map_filter.spec.js
+++ cypress/integration/map_filter.spec.js
@@ -1,29 +1,44 @@
-
 describe('Filter Selection', () => {
 
-  it('Map: Last Week, Dead Animal [DAN]', () => {
-    cy.visit('/')
-    cy.get('#root > header > div.navbar-brand > a').click()
-    cy.get('#root > div > div.menu-container > div.menu-tabs > a:nth-child(1)').click()
-    cy.get('.dropdown-trigger > .button').click()
-    cy.get('[value="LAST_WEEK"]').click()
-    cy.get(':nth-child(2) > .select-group-content > .select-item > .select-item-box').click()
-    cy.get('#type-selector-container > div.columns.is-0 > div:nth-child(1) > div:nth-child(2) > span > input').click({force: true})
-    cy.get('#btn-sidebar-submit-button').click()
-    cy.get('#icon-request-icon-DAN').should("exist");
-  });
+    it('Map: Last Week, Dead Animal [DAN]', () => {
 
-  it('Data Visualization: Last Week, Dead Animal [DAN]', () => {
-    cy.visit('/')
-    cy.get('#root > header > div.navbar-brand > a').click()
-    cy.get('#root > div > div.menu-container > div.menu-tabs > a:nth-child(2)').click()
-    cy.get('.dropdown-trigger > .button').click()
-    cy.get('[value="LAST_WEEK"]').click()
-    cy.get(':nth-child(2) > .select-group-content > .select-item > .select-item-box').click()
-    cy.get('#type-selector-container > div.columns.is-0 > div:nth-child(1) > div:nth-child(2) > span > input').click({force: true})
-    cy.get('#btn-sidebar-submit-button').click()
-    cy.get('#icon-request-icon-DAN').should("exist");
-  });
+        cy.server()
+        cy.route({
+            method: 'POST',
+            url: '/map/clusters',
+            onResponse(xhr) {
+                expect(xhr.status).to.eq(200)
+            }
+        }).as('getFilterList')
+
+        cy.visit('/')
+        cy.get('#root > header > div.navbar-brand > a').click()
+        cy.get('#root > div > div.menu-container > div.menu-tabs > a:nth-child(1)').click()
+        cy.get('.dropdown-trigger > .button').click()
+        cy.get('[value="LAST_WEEK"]').click()
+        cy.get(':nth-child(2) > .select-group-content > .select-item > .select-item-box').click()
+        cy.get('#type-selector-container > div.columns.is-0 > div:nth-child(1) > div:nth-child(2) > span > input').click({force: true})
+        cy.get('#btn-sidebar-submit-button').click()
+
+        cy.wait('@getFilterList').should((xhr) => {
+            const myCount = xhr.responseBody[0].count
+            cy.get('#root > div.body.menu-is-open > div.map-container > div > div.leaflet-pane.leaflet-map-pane > div.leaflet-pane.leaflet-marker-pane > div > div > span').should('have.text', myCount)
+            cy.get('#icon-request-icon-DAN').should("exist")
+        })
+
+    });
+
+    it.skip('Data Visualization: Last Week, Dead Animal [DAN]', () => {
+        cy.visit('/')
+        cy.get('#root > header > div.navbar-brand > a').click()
+        cy.get('#root > div > div.menu-container > div.menu-tabs > a:nth-child(2)').click()
+        cy.get('.dropdown-trigger > .button').click()
+        cy.get('[value="LAST_WEEK"]').click()
+        cy.get(':nth-child(2) > .select-group-content > .select-item > .select-item-box').click()
+        cy.get('#type-selector-container > div.columns.is-0 > div:nth-child(1) > div:nth-child(2) > span > input').click({force: true})
+        cy.get('#btn-sidebar-submit-button').click()
+        cy.get('#icon-request-icon-DAN').should("exist");
+    });
 
 })
 
