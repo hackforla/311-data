bb06dd442c8536a80bec1043fd808985e88553b5
croor005 <richard.crook@disney.com>
2020-07-27 16:13:12 -0700

    For Happy Returns updated

--- cypress/integration/happy_returns.spec.js
+++ cypress/integration/happy_returns.spec.js
@@ -1,16 +1,6 @@
 describe('Working w/ JSON and REST', () => {
 
-    it('Contact Us: Sending Email', () => {
-
-        cy.server()
-        cy.route({
-            method: 'GET',
-            url: '**/getProductVariants',
-            json:true,
-            onResponse(xhr) {
-                expect(xhr.status).to.eq(200)
-            }
-        }).as('getProductVariants')
+    it.skip('JSON Response', () => {
 
        cy.request('https://happyreturnsqatest.proxy.beeceptor.com/getProductVariants').then((response)=>{
            let bodyHr = JSON.stringify(response.body);
@@ -28,5 +18,11 @@ describe('Working w/ JSON and REST', () => {
 
     })
 
+    it('Google Search', ()=> {
+        cy.visit('http://www.google.com')
+        cy.get('#tsf > div:nth-child(2) > div.A8SBwf > div.RNNXgb > div > div.a4bIc > input').type('Happy Returns')
+        cy.get('#tsf > div:nth-child(2) > div.A8SBwf.emcav > div.UUbT9 > div.aajZCb > div.tfB0Bf > center > input.gNO89b').click()
+        cy.get('#rso > div:nth-child(1) > div > div > div.r > a').should('contain.text', 'Happy Returns')
+    })
 
 })
