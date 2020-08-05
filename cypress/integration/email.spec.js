de32331664741a8962cd939c90b2ebaab83ba6f8
croor005 <richard.crook@disney.com>
2020-08-04 18:30:37 -0700

    Adding context Using a stub instead of sending emails

--- cypress/integration/email.spec.js
+++ cypress/integration/email.spec.js
@@ -1,26 +1,42 @@
-describe('Contact Us', () => {
+describe('311 Data', () => {
 
-    const serverId = 'eq9vw94r';
-    const testEmail = `test_user.${serverId}@mailosaur.io`
+    const serverId = '';
 
-    it.skip('Contact Us: Sending Email', () => {
-        cy.visit('/')
-        cy.get(':nth-child(3) > a').click()
-        cy.get('#contact-firstname').type('Test')
-        cy.get('#contact-lastname').type('User')
-        cy.get('#contact-email').type(testEmail)
-        cy.get('#contact-message').type('email test')
-        cy.get('#btn-contact-submit').click()
+    const testEmail = `test_user\@fakeaddress.com`
 
-        cy.get('.contact-success-popup').should('exist')
-    })
+    context('Contct Us', () => {
+        it('Contact Us: Sending Email', () => {
 
-    it.skip('Contact Us: Checking email', () => {
-        cy.mailosaurGetMessage(serverId, {
-            sentTo: testEmail
-        }).then(email => {
-            expect(email.subject).to.equal('Thanks for your feedback');
+            cy.server()
+            cy.route({
+                method:'POST',
+                url: '/feedback',
+                response: [{"success": true}],
+                onResponse(xhr) {
+                    expect(xhr.status).to.eq(200);
+                }
+            }).as('feedbackJSON')
+
+            cy.visit('/')
+            cy.get('.navbar-end > :nth-child(4)').click()
+            cy.get('#contact-firstname').type('Test')
+            cy.get('#contact-lastname').type('User')
+            cy.get('#contact-email').type(testEmail)
+            cy.get('#contact-message').type('email test')
+            cy.get('#btn-contact-submit').click()
+            cy.wait('@feedbackJSON')
+
+            cy.get('.contact-success-popup').should('exist')
+        })
+
+        it.skip('Contact Us: Checking email', () => {
+            cy.mailosaurGetMessage(serverId, {
+                sentTo: testEmail
+            }).then(email => {
+                expect(email.subject).to.equal('Thanks for your feedback');
+            })
         })
     })
+
 
 })
