4bcc8be4ed83ae9ed5465b0bc197eeecfbbc291a
Hannah Livnat <hannahlivnat@gmail.com>
2020-08-15 17:59:19 -0700

    deleted extra spaces

--- client/components/main/menu/Submit.jsx
+++ client/components/main/menu/Submit.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { connect } from 'react-redux';
 import propTypes from 'proptypes';
 import { useLocation } from 'react-router-dom';
