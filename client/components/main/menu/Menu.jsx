1f8498c40a1975866cb9583e90659d19fbfab320
tan-nate <nktan93@gmail.com>
2020-08-17 21:38:46 -0700

    ToggleSwitch kind of works but styling is off

--- client/components/main/menu/Menu.jsx
+++ client/components/main/menu/Menu.jsx
@@ -33,6 +33,7 @@ const Menu = ({
   toggleMenu,
   setMenuTab,
   setMenuMode,
+  toggleComparing,
   isComparing,
 }) => {
   const tabs = [
@@ -45,174 +46,127 @@ const Menu = ({
     MENU_MODES.TRENDS,
   ];
 
-  return (
-    <div className="menu-container">
-      <Switch>
-        <Route exact path="/data">
-          <div className="menu-tabs">
-            {modes.map(mode => (
-              <a
-                key={mode}
-                className={clx('menu-tab', { active: mode === activeMode })}
-                onClick={mode === activeMode ? undefined : () => {
-                  setMenuMode(mode)
-                  if (mode === MENU_MODES.OPEN)
-                    setMenuTab(MENU_TABS.MAP)
-                }}
-              >
-                { mode }
-              </a>
-            ))}
-          </div>
-        </Route>
-      </Switch>
+  if (isComparing) {
+    return (
+      <div className="menu-container">
+        <div className="menu-toggle-button-container">
+          <HoverOverInfo
+            text="Click to show/hide the filters"
+            position="right"
+          >
+            <Button
+              id="menu-toggle-button"
+              icon={!isOpen ? 'chevron-right' : 'chevron-left'}
+              iconStyle={{ margin: '0px' }}
+              handleClick={() => toggleMenu()}
+              color="light"
+            />
+          </HoverOverInfo>
+        </div>
 
-      <div className="menu-toggle-button-container">
-        <HoverOverInfo
-          text="Click to show/hide the filters"
-          position="right"
-        >
-          <Button
-            id="menu-toggle-button"
-            icon={!isOpen ? 'chevron-right' : 'chevron-left'}
-            iconStyle={{ margin: '0px' }}
-            handleClick={() => toggleMenu()}
-            color="light"
+        <div className="menu-content">
+          <InfoTitle
+            title="Compare Different Councils"
+            element="h2"
+            infoText={[
+              'This toggle switch allows the user to choose whether to compare data from different councils.',
+              '* Please click to toggle.',
+            ]}
+            position="bottom"
           />
-        </HoverOverInfo>
+          <ToggleSwitch 
+            handleClick={() => toggleComparing()}
+          />
+          <InfoTitle
+            title="Date Range Selection"
+            element="h2"
+            infoText={[
+              'This filter allows the user to choose a date range for 311 comparison data.',
+              '* Please click to make a selection.',
+            ]}
+          />
+          <DateSelector comparison key="comparison-dateselector" />
+          <InfoTitle
+            title="District Selection"
+            element="h2"
+            infoText={[
+              'This filter allows the user to select specific district boundaries for comparison.',
+              '* Please click to select districts for comparison.',
+            ]}
+          />
+          <DistrictSelector />
+          <InfoTitle
+            title="Chart Selection"
+            element="h2"
+            infoText={[
+              'This filter allows the user to select a chart for comparison.',
+              '* Please click on a chart type to make a selection.',
+            ]}
+          />
+          <ChartSelector />
+          <InfoTitle
+            title="Request Type Selection"
+            element="h2"
+            infoText={[
+              'This filter allows the user to select specific 311 request types for comparison.',
+              '* Please check box to make one or more selections.',
+            ]}
+          />
+          <RequestTypeSelector comparison />
+          <Submit />
+        </div>
       </div>
-
-      <Switch>
-        <Route path="/comparison">
-          <div className="menu-content">
-            <InfoTitle
-              title="Compare Different Councils"
-              element="h2"
-              infoText={[
-                'This toggle switch allows the user to choose whether to compare data from different councils.',
-                '* Please click to toggle.',
-              ]}
-              position="bottom"
-            />
-            <ToggleSwitch 
-              handleClick={() => toggleComparing()}
-            />
-            <InfoTitle
-              title="Date Range Selection"
-              element="h2"
-              infoText={[
-                'This filter allows the user to choose a date range for 311 comparison data.',
-                '* Please click to make a selection.',
-              ]}
-            />
-            <DateSelector comparison key="comparison-dateselector" />
-            <InfoTitle
-              title="District Selection"
-              element="h2"
-              infoText={[
-                'This filter allows the user to select specific district boundaries for comparison.',
-                '* Please click to select districts for comparison.',
-              ]}
-            />
-            <DistrictSelector />
-            <InfoTitle
-              title="Chart Selection"
-              element="h2"
-              infoText={[
-                'This filter allows the user to select a chart for comparison.',
-                '* Please click on a chart type to make a selection.',
-              ]}
-            />
-            <ChartSelector />
-            <InfoTitle
-              title="Request Type Selection"
-              element="h2"
-              infoText={[
-                'This filter allows the user to select specific 311 request types for comparison.',
-                '* Please check box to make one or more selections.',
-              ]}
-            />
-            <RequestTypeSelector comparison />
-            <Submit />
-          </div>
-        </Route>
-        <Route path="/data">
-          {(() => {
-            switch(activeMode) {
-              case MENU_MODES.OPEN: return (
-                <div className="menu-content">
-                  <InfoTitle
-                    title="Address/NC/CC Selection"
-                    element="h2"
-                    infoText={[
-                      'Choose address to search by address, NC to search by neighborhood council, or CC to search by city council.'
-                    ]}
-                    position='bottom'
-                  />
-                  <div style={{ marginTop: 30 }} />
-                  <InfoTitle
-                    title="Request Type Selection"
-                    element="h2"
-                    infoText={[
-                      'This filter allows the user to select specific 311 request types.',
-                      '* Please check box to make one or more selections.',
-                    ]}
-                    position='bottom'
-                  />
-                  <RequestTypeSelector />
-                </div>
-              );
-              case MENU_MODES.TRENDS: return (
-                <div className="menu-content with-tabs">
-                  {/*<h1>Filters</h1>*/}
-                  <InfoTitle
-                    title="Date Range Selection"
-                    element="h2"
-                    infoText={[
-                      'This filter allows the user to choose a date range for 311 data.',
-                      '* Please click to make a selection.',
-                    ]}
-                  />
-                  <DateSelector key="data-dateselector" />
-                  <InfoTitle
-                    title="NC/CC Selection"
-                    element="h2"
-                    infoText={[
-                      'This filter allows the user to select specific neighborhood councils.',
-                      '* Please check box to make one or more selections.',
-                    ]}
-                    position="top"
-                  />
-                  <NCSelector />
-                  <InfoTitle
-                    title="Request Type Selection"
-                    element="h2"
-                    infoText={[
-                      'This filter allows the user to select specific 311 request types.',
-                      '* Please check box to make one or more selections.',
-                    ]}
-                  />
-                  <RequestTypeSelector />
-                  <div className="menu-tabs" style={{ marginTop: 25 }}>
-                    {tabs.map(tab => (
-                      <a
-                        key={tab}
-                        className={clx('menu-tab', { active: tab === activeTab })}
-                        onClick={tab === activeTab ? undefined : () => setMenuTab(tab)}
-                      >
-                        { tab }
-                      </a>
-                    ))}
-                  </div>
-                </div>
-              )
-            }
-          })}
-        </Route>
-      </Switch>
-    </div>
-  );
-};
+    )
+  } else {
+    return (
+      <div className="menu-content with-tabs">
+        {/*<h1>Filters</h1>*/}
+        <ToggleSwitch 
+          handleClick={() => toggleComparing()}
+        />
+        <InfoTitle
+          title="Date Range Selection"
+          element="h2"
+          infoText={[
+            'This filter allows the user to choose a date range for 311 data.',
+            '* Please click to make a selection.',
+          ]}
+        />
+        <DateSelector key="data-dateselector" />
+        <InfoTitle
+          title="NC/CC Selection"
+          element="h2"
+          infoText={[
+            'This filter allows the user to select specific neighborhood councils.',
+            '* Please check box to make one or more selections.',
+          ]}
+          position="top"
+        />
+        <NCSelector />
+        <InfoTitle
+          title="Request Type Selection"
+          element="h2"
+          infoText={[
+            'This filter allows the user to select specific 311 request types.',
+            '* Please check box to make one or more selections.',
+          ]}
+        />
+        <RequestTypeSelector />
+        <div className="menu-tabs" style={{ marginTop: 25 }}>
+          {tabs.map(tab => (
+            <a
+              key={tab}
+              className={clx('menu-tab', { active: tab === activeTab })}
+              onClick={tab === activeTab ? undefined : () => setMenuTab(tab)}
+            >
+              { tab }
+            </a>
+          ))}
+        </div>
+      </div>
+    )
+  }
+}
 
 const mapStateToProps = state => ({
   isOpen: state.ui.menu.isOpen,
@@ -237,6 +191,7 @@ Menu.propTypes = {
   toggleMenu: PropTypes.func,
   setMenuTab: PropTypes.func,
   setMenuMode: PropTypes.func,
+  toggleComparing: PropTypes.func,
   isComparing: PropTypes.bool.isRequired,
 };
 
@@ -244,4 +199,6 @@ Menu.defaultProps = {
   toggleMenu: () => null,
   setMenuTab: () => null,
   setMenuMode: PropTypes.func,
+  toggleComparing: () => null,
+  isComparing: false,
 };
