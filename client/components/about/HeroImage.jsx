803446aa41e1d356c525b44ab941a24116ed9072
tan-nate <nktan93@gmail.com>
2020-08-19 00:58:12 -0700

    updated HeroImage with logos

--- client/components/about/HeroImage.jsx
+++ client/components/about/HeroImage.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 
 const HeroImage = () => (
   <div className="about-hero">
-    <div className="overlay-text">
-      <span className="text-about">About&nbsp;</span>
-      <span className="text-311">311</span>
-      <span className="text-data">DATA</span>
+    <div className="logos">
+        <img id="main-logo" src={require('../../assets/311-data-logo-background.png')} alt="311-Data logo"></img>
+        <div className="presented-by-container">
+          <h2>presented by</h2>
+          <img id="hackforla-main-logo" src={require('../../assets/hackforla.png')} alt="Hack for LA logo"></img>
+        </div>
     </div>
   </div>
 );
