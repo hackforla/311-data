76a224eb5bbc79fd6bf4acafe2c4c80dd3e44033
Matthew Webster <mattwebster@matts-mpb.attlocal.net>
2020-08-24 20:49:16 -0700

    change to import in tests

--- server/api/tests/__init__.py
+++ server/api/tests/__init__.py
@@ -1,10 +1,13 @@
 import os
 import sys
 from os.path import join, dirname
-from settings import Server
 
 
 sys.path.append(join(dirname(__file__), '../src'))
 
-if Server.DEBUG:
-    print(os.environ)
+try:
+    from settings import Server
+    if Server.DEBUG:
+        print(os.environ)
+except ImportError:
+    pass
