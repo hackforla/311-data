58cebc3e756e0c7933d0d021e524ade9b183ece3
Adam Kendis <atkendis@gmail.com>
2020-08-27 15:16:31 -0700

    development and production config files

---
+++ client/config/config.development.js
@@ -0,0 +1,6 @@
+var APP_CONFIG = Object.freeze({
+  // Toggles alt/primary request type colors
+  altRequestColors: true,
+  // Enables/disables Mapbox vector layer in Leaflet map
+  vectorLayersEnabled: true,
+});
---
+++ client/config/config.production.js
@@ -0,0 +1,6 @@
+var APP_CONFIG = Object.freeze({
+  // Toggles alt/primary request type colors
+  altRequestColors: false,
+  // Enables/disables Mapbox vector layer in Leaflet map
+  vectorLayersEnabled: false,
+});
