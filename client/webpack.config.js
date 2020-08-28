55ae8d0ba108106d07a31c122dbfc4b2612fbcc6
Adam Kendis <atkendis@gmail.com>
2020-08-27 17:06:57 -0700

    deleted unused file

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -14,7 +14,6 @@ const env = process.env.NODE_ENV;
 const configFilename = `config.${env}.js`
 // const configFilename = `config.${env}.${crypto.randomBytes(15).toString('hex')}.js`
 
-
 module.exports = {
   mode: env,
   entry: './index.js',
