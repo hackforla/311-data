11b9441cd1536b0c01f775036c5149e36abc35a2
Matthew Webster <mattwebster@matts-mpb.attlocal.net>
2020-09-02 23:49:27 -0700

    set test default config

--- server/api/tests/__init__.py
+++ server/api/tests/__init__.py
@@ -2,6 +2,14 @@ import os
 import sys
 from os.path import join, dirname
 
+if os.getenv("DATABASE_URL") is None:
+    os.environ["DATABASE_URL"] = "postgresql://311_user:311_pass@localhost:5433/311_db"
+
+os.environ["PICKLEBASE_ENABLED"] = "False"
+os.environ["PICKLECACHE_ENABLED"] = "False"
+os.environ["TMP_DIR"] = "./__tmp__"
+
+print(os.environ)
 
 sys.path.append(join(dirname(__file__), '../src'))
 
