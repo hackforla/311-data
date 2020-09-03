0a8f00e3433f59590247c7f167948542117de10d
tan-nate <nktan93@gmail.com>
2020-09-03 01:59:26 -0700

    navbar burger

--- client/components/main/header/Header.jsx
+++ client/components/main/header/Header.jsx
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link, NavLink } from 'react-router-dom';
+import clx from 'classnames';
 import COLORS from '../../../styles/COLORS';
 
 const Header = () => {
+  const [activeBurger, setActiveBurger] = useState(false);
+
   const cta2Style = {
     color: COLORS.BRAND.CTA2,
   };
@@ -15,8 +18,12 @@ const Header = () => {
     color: COLORS.BRAND.CTA1,
   };
 
+  const handleClick = () => {
+    setActiveBurger(!activeBurger);
+  };
+
   return (
-    <header
+    <nav
       className="navbar"
       role="navigation"
       aria-label="main navigation"
@@ -25,10 +32,15 @@ const Header = () => {
         <Link to="/" className="navbar-item">
           <img id="navbar-logo" src={require('../../../assets/311-data-logo.png')} alt="311-Data logo"></img>
         </Link>
+        <a role="button" tabIndex={0} className={clx('navbar-burger', { 'is-active': activeBurger })} aria-label="menu" aria-expanded="false" onClick={handleClick} onKeyDown={handleClick}>
+          <span aria-hidden="true" />
+          <span aria-hidden="true" />
+          <span aria-hidden="true" />
+        </a>
       </div>
 
-      <div id="navbar" className="navbar-menu">
-        <div className="navbar">
+      <div className={clx('navbar-menu', { 'is-active': activeBurger })}>
+        <div className="navbar-start">
           <div className="navbar-item">
             <NavLink exact to="/data" activeClassName="navbar-selected" style={backgroundStyle}>
               Explore My Council&apos;s 311 Data
@@ -40,7 +52,7 @@ const Header = () => {
             </NavLink>
           </div>
         </div>
-        <div id="navbar" className="navbar-end">
+        <div className="navbar-end">
           <div className="navbar-item">
             <NavLink exact to="/about" activeClassName="navbar-selected" style={backgroundStyle}>
               About 311 Data
@@ -53,7 +65,7 @@ const Header = () => {
           </div>
         </div>
       </div>
-    </header>
+    </nav>
   );
 };
 
