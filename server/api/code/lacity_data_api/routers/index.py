1dcf5f52b2cd5ba89d01171ff8df9a217cfd7053
Matthew Webster <mattwebster@matts-mpb.attlocal.net>
2020-09-08 15:16:45 -0700

    disabling cache build

--- server/api/code/lacity_data_api/routers/index.py
+++ server/api/code/lacity_data_api/routers/index.py
@@ -1,11 +1,11 @@
 from fastapi import APIRouter
 
-from .utilities import build_cache
+# from .utilities import build_cache
 
 router = APIRouter()
 
 
 @router.get("/")
 async def index():
-    await build_cache()
+    # await build_cache()
     return {"message": "Hello, new index!"}
