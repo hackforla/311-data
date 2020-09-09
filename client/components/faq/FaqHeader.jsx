7fc77d5506ee4ebd57f58e2b61f3da59bb199c0f
Hannah Livnat <hannahlivnat@gmail.com>
2020-09-09 16:30:46 -0700

    work on autocomplete, incomplete

--- client/components/faq/FaqHeader.jsx
+++ client/components/faq/FaqHeader.jsx
@@ -1,11 +1,13 @@
 import React, {useState} from 'react';
 import PropTypes from 'proptypes';
 import { connect } from 'react-redux';
+import { FAQS } from '@components/common/FaqContent.js';
 
 const FaqHeader = ({
 updateSearch
 }) => {
   const [searchInput, updateSearchInput] = useState("");
+  const faqSearchAutoComplete = FAQS.map((item) => {return item.question});
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -13,16 +15,41 @@ updateSearch
   }
 
   const handleChange = (e) => {
-    updateSearchInput(e.target.value);
-    console.log(searchInput)
+    let val = e.target.value;
+    updateSearchInput(val);
+
+    if(val.length == 0) {
+      return false;
+    }
+    document.querySelector('.autocomplete-items').style.display = 'none';
+    console.log
+
+    for (let searchOption of faqSearchAutoComplete) {
+      if (searchOption.toLowerCase().match(/`${val.toLowerCase()}`/)) {
+        let b = document.createElement("div");
+        b.innerHTML = "<strong>" + searchOption.substr(0, val.length) + "</strong>";
+        b.innerHTML += searchOption.substr(val.length); 
+
+        b.innerHTML += "<input type='hidden' value='"+ searchOption +"'>";
+        b.addEventListener("click", function(e) {
+          val = this.getElementsByTagName("input")[0].value;
+          closeAllLists();
+        });
+        a.appendChild(b);
+      }
+    }
+    
   }
 
   return (
     <div className="faq-header">
       <h1>What can we help you with? </h1>
-      <form onSubmit={handleSubmit}>
-        <input onChange={handleChange} name="search" type="text" placeholder="Type your question here..."/>
-        <input type="submit" />
+      <form autoComplete="off" onSubmit={handleSubmit}>
+        <div className="autocomplete">
+          <input onChange={handleChange} name="search" type="text" placeholder="Type your question here..." />
+          <div id="autocomplete-list" className="autocomplete-items"></div>
+        </div>
+        <input type="submit" value="Search"/>
       </form>
     </div>
   )
