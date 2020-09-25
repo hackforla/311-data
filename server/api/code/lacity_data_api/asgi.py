1ecdfd395b949def30f47ce6883795eafe4e76b1
Matthew Webster <mattwebster@matts-mpb.attlocal.net>
2020-09-25 16:36:05 -0700

    using gunicorn for API

--- server/api/code/lacity_data_api/asgi.py
+++ server/api/code/lacity_data_api/asgi.py
@@ -1,13 +1,16 @@
 import sentry_sdk
 from sentry_sdk.integrations.asgi import SentryAsgiMiddleware
+from sentry_sdk.integrations.sqlalchemy import SqlalchemyIntegration
 
 from .main import get_app
+
 
 app = get_app()
 
 sentry_sdk.init(
     "https://467b42786c584f87ae7a7760f96b5652@o452210.ingest.sentry.io/5439266",
-    traces_sample_rate=1.0
+    traces_sample_rate=1.0,
+    integrations=[SqlalchemyIntegration()]
 )
 
 app = SentryAsgiMiddleware(app)
