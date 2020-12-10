7297e9325b74ec12045505ec6fc56011dc5b89cc
Adam Kendis <atkendis@gmail.com>
2020-12-10 15:15:35 -0800

    theme updates, applied to Header, Footer

--- client/theme/theme.js
+++ client/theme/theme.js
@@ -3,6 +3,26 @@ import { createMuiTheme } from '@material-ui/core/styles';
 const theme = createMuiTheme({
   palette: {
     type: 'dark',
+    primary: {
+      main: '#2A404E',
+      dark: '#192730',
+    },
+    secondary: {
+      main: '#0F181F',
+    },
+    background: {
+      default: '#1A1A1A',
+    },
+    typography: {
+      fontFamily: ['Oswald', 'sans-serif'],
+      dark: '#C4C4C4',
+    },
+    header: {
+      height: '62px',
+    },
+    footer: {
+      height: '40px',
+    }
   },
 });
 
