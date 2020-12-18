d4a38901822e3212bedd05cdd58e5da3d1f936d4
Adam Kendis <atkendis@gmail.com>
2020-12-17 16:56:12 -0800

    clean up comments

--- client/Routes.jsx
+++ client/Routes.jsx
@@ -1,28 +1,14 @@
-/* eslint-disable */
-// temporarily disabling eslint here until v2 refactor
 import React from 'react';
 import {
   Switch,
   Route,
   Redirect,
 } from 'react-router-dom';
-
-// import PrivacyPolicy from '@components/privacyPolicy/PrivacyPolicy';
-// import Contact from './components/contact/Contact';
-// import About from './components/about/About';
-// import Body from './components/main/body/Body';
-// import Faq from './components/faq/Faq';
-import Desktop from '@components/main/Desktop'
+import Desktop from '@components/main/Desktop';
 
 export default function Routes() {
   return (
     <Switch>
-      {/* <Route path="/contact" component={Contact} /> */}
-      {/* <Route path="/about" component={About} /> */}
-      {/* <Route path="/privacy" component={PrivacyPolicy} /> */}
-      {/* <Route path="/comparison" component={Body} /> */}
-      {/* <Route path="/faq" component={Faq} /> */}
-      {/* <Route path="/data" component={Body} /> */}
       <Route path="/map" component={Desktop} />
       <Route path="/">
         <Redirect to="map" />
