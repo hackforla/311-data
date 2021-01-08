90eb2daa33e2a4c59d9e78170805244fc621636d
Rufat Aliyev <46456022+rufataliy@users.noreply.github.com>
2021-01-08 17:22:57 -0500

    Added theme colors constants into colors.js

--- client/theme/colors.js
+++ client/theme/colors.js
@@ -1,5 +1,22 @@
-const primaryTextColor = '#FFFFFF';
+export const color_primary_dark = '#29404F';
+export const color_primary_focus = '#FFB100';
+export const color_secondary_dark = '#0F181F';
+export const color_secondary_focus = '#87C8BC';
+export const color_text_primary_dark = '#0F181F'
+export const color_text_primary_light = '#FFFFFF'
+export const color_text_secondary_dark = '#0F181F'
+export const color_text_secondary_light = '#ECECEC'
+export const color_text_focus = '#87C8BC'
+
 
 export default {
-  primaryTextColor,
+  color_primary_dark,
+  color_primary_focus,
+  color_secondary_dark,
+  color_secondary_focus,
+  color_text_primary_dark,
+  color_text_primary_light,
+  color_text_secondary_dark,
+  color_text_secondary_light,
+  color_text_focus
 };
