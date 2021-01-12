f959be3e8ed85386e4416a9c74f1f9f2ca27495b
Rufat Aliyev <46456022+rufataliy@users.noreply.github.com>
2021-01-12 14:42:15 -0500

    Added Display sub component

--- client/components/common/SelectorBox.jsx
+++ client/components/common/SelectorBox.jsx
@@ -1,6 +1,11 @@
 import React, { useState, useContext } from "react";
 import PropTypes from "proptypes";
+
+import ArrowDropDownIcon from "@material-ui/icons/ArrowDropDown";
+import ArrowDropUpIcon from "@material-ui/icons/ArrowDropUp";
+import IconButton from "@material-ui/core/IconButton";
 import Card from "@material-ui/core/Card";
+import CardHeader from "@material-ui/core/CardHeader";
 import CardContent from "@material-ui/core/CardContent";
 import CollapseMUI from "@material-ui/core/Collapse";
 
@@ -10,6 +15,7 @@ const SelectorBox = ({
   onToggle,
   children,
   expanded: initial,
+  arrowHidden,
 }) => {
   const [expanded, setExpanded] = useState(initial);
 
@@ -17,15 +23,46 @@ const SelectorBox = ({
     onToggle && onToggle();
     setExpanded((prevState) => !prevState);
   };
-
+  const renderDisplay = () => {
+    const element = children.find((child) => child.type.name === "Display");
+    return (
+      <CardHeader
+        disableTypography
+        classes={{
+          root: classes.header,
+          action: classes.headerAction,
+        }}
+        title={element}
+        {...(!arrowHidden
+          ? {
+              action: (
+                <IconButton
+                  className={classes.button}
+                  aria-label="toggle filter menu"
+                  onClick={toggleCollapse}
+                  disableFocusRipple
+                  disableRipple
+                >
+                  {expanded ? (
+                    <ArrowDropUpIcon className={classes.button} />
+                  ) : (
+                    <ArrowDropDownIcon className={classes.button} />
+                  )}
+                </IconButton>
+              ),
+            }
+          : {})}
+      />
+    );
+  };
   const renderCollapse = () => {
     const element = children.find((child) => child.type.name === "Collapse");
     return element;
   };
-  
   return (
     <Context.Provider value={{ expanded }}>
-      <Card>
+      <Card >
+        {renderDisplay()}
         {renderCollapse()}
       </Card>
     </Context.Provider>
@@ -41,7 +78,12 @@ SelectorBox.defaultProps = {
   expanded: false,
 };
 
+SelectorBox.Display = Display;
 SelectorBox.Collapse = Collapse;
+
+function Display({ children }) {
+  return <div>{children}</div>;
+}
 
 function Collapse({ children }) {
   const { expanded } = useContext(Context);
