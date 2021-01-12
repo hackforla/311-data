842aa871881482db8465bb69e1d4f5d034272460
Rufat Aliyev <46456022+rufataliy@users.noreply.github.com>
2021-01-12 15:20:10 -0500

    Applied linitng rules

--- client/components/common/SelectorBox.jsx
+++ client/components/common/SelectorBox.jsx
@@ -1,14 +1,57 @@
-import React, { useState, useContext } from "react";
-import PropTypes from "proptypes";
+import React, { useState, useContext } from 'react';
+import PropTypes from 'proptypes';
 
-import ArrowDropDownIcon from "@material-ui/icons/ArrowDropDown";
-import ArrowDropUpIcon from "@material-ui/icons/ArrowDropUp";
-import IconButton from "@material-ui/core/IconButton";
-import Card from "@material-ui/core/Card";
-import CardHeader from "@material-ui/core/CardHeader";
-import CardContent from "@material-ui/core/CardContent";
-import CollapseMUI from "@material-ui/core/Collapse";
-import { makeStyles } from "@material-ui/core/styles";
+import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown';
+import ArrowDropUpIcon from '@material-ui/icons/ArrowDropUp';
+import IconButton from '@material-ui/core/IconButton';
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardContent from '@material-ui/core/CardContent';
+import CollapseMUI from '@material-ui/core/Collapse';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles(theme => ({
+  card: {
+    backgroundColor: theme.palette.primary.main,
+    boxShadow: 'none',
+  },
+  header: {
+    backgroundColor: theme.palette.primary.dark,
+    color: theme.palette.text.primary,
+    padding: 5,
+    paddingLeft: 10,
+    fontSize: '1rem',
+    marginBottom: 2,
+    borderRadius: 5,
+  },
+  content: {
+    borderRadius: 5,
+    backgroundColor: theme.palette.primary.dark,
+    padding: '10px 10px',
+  },
+  headerAction: {
+    margin: 'auto',
+  },
+  headerTitle: {
+    marginLeft: 10,
+    fontSize: 20,
+    fontWeight: 600,
+    letterSpacing: '2px',
+  },
+  button: {
+    padding: '0 0 0 5px',
+    '&:hover': {
+      backgroundColor: theme.palette.primary.dark,
+    },
+    '& svg': {
+      fontSize: 30,
+      paddingRight: 0,
+      '&  path': {
+        fill: theme.palette.primary.focus,
+      },
+    },
+  },
+}));
 
 const Context = React.createContext({ expanded: false });
 
@@ -22,11 +65,11 @@ const SelectorBox = ({
   const classes = useStyles();
 
   const toggleCollapse = () => {
-    onToggle && onToggle();
-    setExpanded((prevState) => !prevState);
+    if (onToggle) onToggle();
+    setExpanded(prevState => !prevState);
   };
   const renderDisplay = () => {
-    const element = children.find((child) => child.type.name === "Display");
+    const element = children.find(child => child.type.name === 'Display');
     return (
       <CardHeader
         disableTypography
@@ -35,30 +78,28 @@ const SelectorBox = ({
           action: classes.headerAction,
         }}
         title={element}
-        {...(!arrowHidden
-          ? {
-              action: (
-                <IconButton
-                  className={classes.button}
-                  aria-label="toggle filter menu"
-                  onClick={toggleCollapse}
-                  disableFocusRipple
-                  disableRipple
-                >
-                  {expanded ? (
-                    <ArrowDropUpIcon className={classes.button} />
-                  ) : (
-                    <ArrowDropDownIcon className={classes.button} />
-                  )}
-                </IconButton>
-              ),
-            }
-          : {})}
+        action={
+          !arrowHidden ? (
+            <IconButton
+              className={classes.button}
+              aria-label="toggle filter menu"
+              onClick={toggleCollapse}
+              disableFocusRipple
+              disableRipple
+            >
+              {expanded ? (
+                <ArrowDropUpIcon className={classes.button} />
+              ) : (
+                <ArrowDropDownIcon className={classes.button} />
+              )}
+            </IconButton>
+          ) : null
+        }
       />
     );
   };
   const renderCollapse = () => {
-    const element = children.find((child) => child.type.name === "Collapse");
+    const element = children.find(child => child.type.name === 'Collapse');
     return element;
   };
   return (
@@ -74,18 +115,28 @@ const SelectorBox = ({
 SelectorBox.propTypes = {
   onToggle: PropTypes.func,
   expanded: PropTypes.bool,
-};
-SelectorBox.defaultProps = {
-  onToggle: undefined,
-  expanded: false,
+  arrowHidden: PropTypes.bool,
+  children: PropTypes.node,
 };
 
-SelectorBox.Display = Display;
-SelectorBox.Collapse = Collapse;
+SelectorBox.defaultProps = {
+  onToggle: undefined,
+  arrowHidden: false,
+  expanded: false,
+  children: null,
+};
 
 function Display({ children }) {
   return <div>{children}</div>;
 }
+
+Display.propTypes = {
+  children: PropTypes.node,
+};
+
+Display.defaultProps = {
+  children: null,
+};
 
 function Collapse({ children }) {
   const { expanded, classes } = useContext(Context);
@@ -97,50 +148,15 @@ function Collapse({ children }) {
   );
 }
 
-function useStyles() {
-  const getStyles = makeStyles((theme) => ({
-    card: {
-      backgroundColor: theme.palette.primary.main,
-      boxShadow: "none",
-    },
-    header: {
-      backgroundColor: theme.palette.primary.dark,
-      color: theme.palette.text.primary,
-      padding: 5,
-      paddingLeft: 10,
-      fontSize: "1rem",
-      marginBottom: 2,
-      borderRadius: 5,
-    },
-    content: {
-      borderRadius: 5,
-      backgroundColor: theme.palette.primary.dark,
-      padding: "10px 10px",
-    },
-    headerAction: {
-      margin: "auto",
-    },
-    headerTitle: {
-      marginLeft: 10,
-      fontSize: 20,
-      fontWeight: 600,
-      letterSpacing: "2px",
-    },
-    button: {
-      padding: "0 0 0 5px",
-      "&:hover": {
-        backgroundColor: theme.palette.primary.dark,
-      },
-      "& svg": {
-        fontSize: 30,
-        paddingRight: 0,
-        "&  path": {
-          fill: theme.palette.primary.focus,
-        },
-      },
-    },
-  }));
-  return getStyles();
-}
+Collapse.propTypes = {
+  children: PropTypes.node,
+};
+
+Collapse.defaultProps = {
+  children: null,
+};
+
+SelectorBox.Display = Display;
+SelectorBox.Collapse = Collapse;
 
 export default SelectorBox;
