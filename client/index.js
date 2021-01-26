ddd6c777e4a564ab2c749676fbafd9365b0c30e8
Rufat Aliyev <46456022+rufataliy@users.noreply.github.com>
2021-01-25 23:17:42 -0500

    Added mock server

--- client/index.js
+++ client/index.js
@@ -1,15 +1,20 @@
 /* eslint-disable react/jsx-filename-extension */
 
-import 'core-js/stable';
-import 'regenerator-runtime/runtime';
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { ThemeProvider } from '@material-ui/core/styles';
-import { CssBaseline } from '@material-ui/core';
-import store from './redux/store';
-import App from './App';
-import theme from './theme/theme';
+import "core-js/stable";
+import "regenerator-runtime/runtime";
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { ThemeProvider } from "@material-ui/core/styles";
+import { CssBaseline } from "@material-ui/core";
+import store from "./redux/store";
+import App from "./App";
+import theme from "./theme/theme";
+
+if (process.env.NODE_ENV === "development") {
+  const { server } = require("./mockServer/server.js");
+  server.start();
+}
 
 ReactDOM.render(
   <Provider store={store}>
@@ -18,7 +23,7 @@ ReactDOM.render(
       <App />
     </ThemeProvider>
   </Provider>,
-  document.getElementById('root'),
+  document.getElementById("root")
 );
 
 // hot module replacement during development
