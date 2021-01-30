4447ab267e334930593209852986e538ab79e72f
Rufat Aliyev <46456022+rufataliy@users.noreply.github.com>
2021-01-30 14:12:18 -0500

    Added more colors to the theme

--- client/components/DateSelector/useStyles.js
+++ client/components/DateSelector/useStyles.js
@@ -1,17 +1,17 @@
-import { makeStyles } from "@material-ui/core/styles";
+import { makeStyles } from '@material-ui/core/styles';
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles(theme => ({
   label: {
     marginBottom: 10,
-    display: "inline-block",
-    fontFamily: "Roboto",
+    display: 'inline-block',
+    fontFamily: 'Roboto',
   },
   selector: {
-    fontFamily: "Roboto",
-    display: "flex",
-    justifyContent: "space-between",
-    alignItems: "center",
-    "& > div": {
+    fontFamily: 'Roboto',
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    '& > div': {
       paddingTop: 0,
       paddingRight: 0,
       paddingBottom: 0,
@@ -21,19 +21,19 @@ const useStyles = makeStyles((theme) => ({
   separator: {
     marginLeft: theme.gaps.md,
     borderRight: `1.5px solid ${theme.palette.text.secondaryLight}`,
-    height: "1.2rem",
+    height: '1.2rem',
   },
   option: {
-    cursor: "pointer",
+    cursor: 'pointer',
     padding: 6,
-    margin: "2px 0",
-    fontFamily: "Roboto",
-    width: "100%",
+    margin: '2px 0',
+    fontFamily: 'Roboto',
+    width: '100%',
     backgroundColor: theme.palette.primary.dark,
-    border: "none",
-    textAlign: "left",
+    border: 'none',
+    textAlign: 'left',
     color: theme.palette.text.secondaryLight,
-    "&:hover": {
+    '&:hover': {
       backgroundColor: theme.palette.selected.primary,
     },
   },
