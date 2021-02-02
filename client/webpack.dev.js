966a1a392053d41758a5e554dadd18e569a85df1
Rufat Aliyev <46456022+rufataliy@users.noreply.github.com>
2021-02-02 14:45:46 -0500

    Added to writeToDisk option to dev server. If not server will try to copy social-media-card-image.png from public to dist which didn't exist and caused looping which resulted in High CPU usage.

--- client/webpack.dev.js
+++ client/webpack.dev.js
@@ -1,12 +1,15 @@
 const merge = require('webpack-merge');
 const config = require('./webpack.config.js');
+const path = require("path")
 
 module.exports = merge(config, {
   mode: 'development',
   devtool: 'inline-source-map',
   devServer: {
     open: true,
-    contentBase: './public',
+    contentBase: path.join(__dirname,'./dist'),
+    publicPath:"/",
+    writeToDisk: true,
     compress: true,
     port: 3000,
     hot: true,
