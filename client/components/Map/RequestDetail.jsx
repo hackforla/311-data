8b3ee6e5badb94e5156b5e177e3e7b4e7b1f4dc4
Adam Kendis <atkendis@gmail.com>
2021-03-11 10:40:56 -0800

    RequestDetail component

--- client/components/Map/RequestDetail.jsx
+++ client/components/Map/RequestDetail.jsx
@@ -1,83 +1,169 @@
-/* eslint-disable */
-
+/* eslint-disable react/prop-types */
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import { withStyles } from '@material-ui/core/styles';
 import moment from 'moment';
 import { getPinInfoRequest } from '@reducers/data';
-import { REQUEST_TYPES, COUNCILS } from '@components/common/CONSTANTS';
+import toTitleCase from '@utils/toTitleCase';
+import Grid from '@material-ui/core/Grid';
+import Divider from '@material-ui/core/Divider';
+import FiberManualRecordIcon from '@material-ui/icons/FiberManualRecord';
+import CircularProgress from '@material-ui/core/CircularProgress';
+
+const styles = theme => ({
+  loader: {
+    margin: 10,
+  },
+  loaderText: {
+    textAlign: 'center',
+    marginTop: -10,
+  },
+  popupContent: {
+    backgroundColor: theme.palette.primary.main,
+    padding: '0 8',
+    minWidth: 215,
+  },
+  requestType: {
+    ...theme.typography.h4,
+    marginRight: 5,
+  },
+  icon: {
+    display: 'block',
+  },
+  divider: {
+    marginTop: 6,
+    marginBottom: 8,
+  },
+  info1: {
+    ...theme.typography.body2,
+    marginTop: 0,
+    marginBottom: 0,
+  },
+  info2: {
+    marginTop: 14,
+  },
+});
 
 class RequestDetail extends React.Component {
   componentDidUpdate(prev) {
-    const { srnumber, pinsInfo, getPinInfo } = this.props;
-    if (srnumber === prev.srnumber)
-      return;
+    const { requestId, pinsInfo, getPinInfo } = this.props;
+    if (requestId === prev.requestId) return;
 
-    if (srnumber && !pinsInfo[srnumber])
-      getPinInfo(srnumber);
+    if (requestId && !pinsInfo[requestId]) getPinInfo(requestId);
   }
 
   render() {
-    const { srnumber, pinsInfo } = this.props;
+    const {
+      classes, requestId, pinsInfo, requestTypes,
+    } = this.props;
 
-    if (!srnumber)
-      return null;
+    if (!requestId) return null;
 
-    if (!pinsInfo[srnumber])
-      return <div>loading</div>
+    if (!pinsInfo[requestId]) {
+      return (
+        <>
+          <CircularProgress
+            className={classes.loader}
+            size={30}
+            color="inherit"
+          />
+          <div className={classes.loaderText}>loading...</div>
+        </>
+      );
+    }
 
     const {
-      requesttype,
+      srnumber,
+      councilName,
+      typeName,
+      typeId: requestTypeId,
+      createdDate,
+      closedDate,
       address,
-      createddate,
-      updateddate,
-      closeddate,
-      status,
-      nc,
-    } = pinsInfo[srnumber];
-
-    const ncName = COUNCILS.find(c => c.id == nc)?.name;
-    const { displayName, color, abbrev } = REQUEST_TYPES[requesttype];
+    } = pinsInfo[requestId];
 
     return (
-      <div className="pin-popup">
-        <p className="pin-popup-type has-text-weight-bold">
-          {displayName}
-          {/*&nbsp;
-          [
-          <span className="pin-popup-type-abbrev" style={{ color }}>
-            {abbrev}
-          </span>
-          ]*/}
-        </p>
-        <p className="pin-popup-ncname">{ncName}</p>
-        <p className="pin-popup-address has-text-weight-bold">{address}</p>
-        <div className="pin-popup-status">
-          <p>
-            Reported on&nbsp;
-            {moment.unix(createddate).format('l')}
-          </p>
-          <p>{srnumber}</p>
-          {/*<p>
-            Status:&nbsp;
-            {status}
-          </p>
+      <div className={classes.popupContent}>
+        <Grid
+          container
+          direction="row"
+          justify="flex-start"
+          alignItems="center"
+        >
+          <Grid className={classes.requestType} item>
+            {typeName}
+          </Grid>
+          <Grid item>
+            <FiberManualRecordIcon
+              className={classes.icon}
+              style={{
+                color: requestTypes.find(({ typeId }) => typeId === requestTypeId).color,
+                fontSize: 20,
+              }}
+            />
+          </Grid>
+        </Grid>
+        <Divider className={classes.divider} />
+        <p className={classes.info1}>{toTitleCase(address)}</p>
+        <p className={classes.info1}>{`${councilName} NC`}</p>
+        <Grid
+          className={classes.info2}
+          container
+          direction="row"
+          justify="space-between"
+          alignItems="flex-start"
+        >
+          <Grid item xs={6}>
+            Service request:
+          </Grid>
+          <Grid
+            item
+            xs={6}
+            style={{ textAlign: 'right' }}
+          >
+            {srnumber}
+          </Grid>
+          <Grid item xs={6}>
+            Reported on:
+          </Grid>
+          <Grid
+            item
+            xs={6}
+            style={{ textAlign: 'right' }}
+          >
+            {moment(createdDate).format('l')}
+          </Grid>
           {
-            closeddate ? (
-              <p>
-                Closed on&nbsp;
-                {moment.unix(closeddate).format('l')}
-              </p>
+            closedDate ? (
+              <>
+                <Grid item xs={6}>
+                  Closed on:
+                </Grid>
+                <Grid
+                  item
+                  xs={6}
+                  style={{ textAlign: 'right' }}
+                >
+                  {moment(closedDate).format('l')}
+                </Grid>
+              </>
             ) : (
               <>
-                <p>
-                  Last updated on&nbsp;
-                  {moment.unix(updateddate).format('l')}
-                </p>
+                <Grid item xs={6}>
+                  Status:
+                </Grid>
+                <Grid
+                  item
+                  xs={6}
+                  style={{ textAlign: 'right' }}
+                >
+                  Open
+                </Grid>
               </>
             )
-          }*/}
-        </div>
+          }
+        </Grid>
       </div>
     );
   }
@@ -85,21 +171,23 @@ class RequestDetail extends React.Component {
 
 const mapStateToProps = state => ({
   pinsInfo: state.data.pinsInfo,
+  requestTypes: state.metadata.requestTypes,
 });
 
 const mapDispatchToProps = dispatch => ({
   getPinInfo: srnumber => dispatch(getPinInfoRequest(srnumber)),
 });
 
-export default connect(mapStateToProps, mapDispatchToProps)(RequestDetail);
+export default connect(mapStateToProps, mapDispatchToProps)(withStyles(styles)(RequestDetail));
 
 RequestDetail.propTypes = {
-  srnumber: PropTypes.string,
+  requestId: PropTypes.number,
   pinsInfo: PropTypes.shape({}),
+  requestTypes: PropTypes.arrayOf(PropTypes.shape({})).isRequired,
   getPinInfo: PropTypes.func.isRequired,
 };
 
 RequestDetail.defaultProps = {
-  srnumber: null,
+  requestId: null,
   pinsInfo: {},
 };
