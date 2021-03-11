2bd4e6fcf23638175301dc67165a33efc5dc3fc5
Adam Kendis <atkendis@gmail.com>
2021-03-11 12:09:40 -0800

    Set Reports div and Backdrop height and position.

--- client/components/main/Reports.jsx
+++ client/components/main/Reports.jsx
@@ -1,9 +1,25 @@
 import React from 'react';
 import { useLocation } from 'react-router-dom';
+import { makeStyles } from '@material-ui/core/styles';
 import { Backdrop, CircularProgress } from '@material-ui/core';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    height: `calc(100vh - ${theme.header.height} - ${theme.footer.height})`,
+    width: '100vw',
+    backgroundColor: 'black',
+  },
+  backdrop: {
+    position: 'absolute',
+    top: theme.header.height,
+    bottom: theme.footer.height,
+    height: `calc(100vh - ${theme.header.height} - ${theme.footer.height})`,
+  },
+}));
 
 const Reports = () => {
   const [isLoading, setIsLoading] = React.useState(true);
+  const classes = useStyles();
 
   const url = process.env.REPORT_URL;
   let reportPath = '/dashboards/overview';
@@ -26,14 +42,14 @@ const Reports = () => {
 
   return (
     <div
-      style={{
-        width: '100%',
-        height: '100%',
-        minHeight: '100%',
-        backgroundColor: 'black',
-      }}
+      className={classes.root}
     >
-      <Backdrop style={{ zIndex: 100 }} open={isLoading} invisible={!isLoading}>
+      <Backdrop
+        className={classes.backdrop}
+        style={{ zIndex: 100 }}
+        open={isLoading}
+        invisible={!isLoading}
+      >
         <CircularProgress />
       </Backdrop>
       <iframe
