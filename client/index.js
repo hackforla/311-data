1ed317cd91fb8efdc97405c43b5f20a127350f60
Matt Webster <me@matt-webster.net>
2021-03-11 21:28:27 -0800

    linting

--- client/index.js
+++ client/index.js
@@ -4,8 +4,8 @@ import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import * as Sentry from "@sentry/react";
-import { Integrations } from "@sentry/tracing";
+import * as Sentry from '@sentry/react';
+import { Integrations } from '@sentry/tracing';
 import { Provider } from 'react-redux';
 import { ThemeProvider } from '@material-ui/core/styles';
 import { CssBaseline } from '@material-ui/core';
@@ -13,9 +13,8 @@ import store from './redux/store';
 import App from './App';
 import theme from './theme/theme';
 
-
 Sentry.init({
-  dsn: "https://7e1f75ca9a3d4de797f3f6cb95478ac9@o452210.ingest.sentry.io/5672572",
+  dsn: 'https://7e1f75ca9a3d4de797f3f6cb95478ac9@o452210.ingest.sentry.io/5672572',
   integrations: [new Integrations.BrowserTracing()],
 
   // Set tracesSampleRate to 1.0 to capture 100%
