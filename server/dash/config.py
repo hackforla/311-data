d833ba01e6374c9e15314ec77968fd794414ae65
Matt Webster <me@matt-webster.net>
2021-04-13 08:39:02 -0700

    Get Dash project set up for new developers Fixes #1073

--- server/dash/config.py
+++ server/dash/config.py
@@ -2,5 +2,5 @@ from os import environ
 
 
 API_HOST = environ.get('API_HOST') or 'https://dev-api.311-data.org'
-PRELOAD = bool(environ.get('PRELOAD'))
+PRELOAD = environ.get('PRELOAD', 'False').lower() in ['true', '1']
 DASH_FILES = environ.get('DASH_FILES') or 'dashboards'
