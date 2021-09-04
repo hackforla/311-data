2898d813a3096da2087105b47fbfd32f1fdde702
funbunch <shannan_bunch@hotmail.com>
2021-09-03 20:03:05 -0700

    Reworked copy and button styles

--- client/components/main/CookieNotice.jsx
+++ client/components/main/CookieNotice.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable */
 import React from 'react';
 import PropTypes from 'prop-types';
 import { makeStyles } from '@material-ui/core/styles';
@@ -14,10 +15,28 @@ import InfoOutlinedIcon from '@material-ui/icons/InfoOutlined';
 const useStyles = makeStyles(theme => ({
   root: {
     width: 325,
+    backgroundColor: "#29404F",
   },
   title: {
-    ...theme.typography.h2
+    paddingTop: 16,
+    paddingLeft: 16,
+    ...theme.typography.h4
   },
+  headStyle: {
+    padding: 10,
+    backgroundColor: "#87C8BC",
+  },
+  iconStyle: {
+    verticalAlign: "bottom"
+  },
+  copyStyle: {
+    paddingTop: 5,
+    fontSize: 12
+  },
+  buttonStyle: {
+    padding: 0,
+    color: "#FFB100"
+  }
 }));
 
 const CookieNotice = ({
@@ -33,12 +52,18 @@ const CookieNotice = ({
   if (showCookieNotice) {
     return (
       <Card className={classes.root}>
-      <CardContent className={classes.title}>
-        <InfoOutlinedIcon/> Cookies & Privacy
+        <CardHeader className={classes.headStyle}></CardHeader>
+        <div className={classes.title}>
+          <InfoOutlinedIcon className={classes.iconStyle} fontSize="small"/> Cookies and Privacy Policy<br />
+        </div>
+      <CardContent className={classes.copyStyle}>
+        We use cookies and other tracking technologies to improve your browsing experience and to better understand our website traffic. By browsing our website, you consent to our use of cookies and other tracking technologies.
+        <Button href="#" className={classes.buttonStyle} size="small"> Learn More</Button>
+        <CardActions style={{ justifyContent: "flex-end" }} >
+        <Button href="#" variant="outlined">Got it!</Button>
+        </CardActions>
       </CardContent>
-      <CardActions>
-        <Button size="small">Learn More</Button>
-      </CardActions>
+  
     </Card>
 
       // <div className="cookie-notice">
