3de735f83ee74ad805390004010c34550015abce
Yi Heng Joshua Wu <joshuayhwu@gmail.com>
2022-07-26 23:14:31 -0700

    Update api path with urlencode

--- server/dash/dashboards/overviewComb.py
+++ server/dash/dashboards/overviewComb.py
@@ -1,5 +1,6 @@
 import textwrap
 import datetime
+import urllib.parse
 import pandas as pd
 import plotly.express as px
 import plotly.graph_objects as go
@@ -9,39 +10,44 @@ from config import API_HOST
 from design import LABELS
 
 TITLE = "OVERVIEW COMBINED DASHBOARD"
-
+REPORT_API_PATH_ROOT = "/reports?"
 # DATA
 
 # Loading the dataframe for the NCs and correspoding requests
-NC_REQ_COUNT_DATA_API_PATH = "/reports?field=council_name&filter=created_date>=2016-01-01"
+nc_req_count_api_params = {'field':'council_name', 'filter':'created_date>=2016-01-01'}
+NC_REQ_COUNT_DATA_API_PATH = REPORT_API_PATH_ROOT + urllib.parse.urlencode(nc_req_count_api_params)
 print(" * Downloading data from API path: " + NC_REQ_COUNT_DATA_API_PATH)
 nc_req_count_df = pd.read_json(API_HOST + NC_REQ_COUNT_DATA_API_PATH)
 nc_req_count_df = nc_req_count_df.groupby(['council_name'])['counts'].sum().sort_values().to_frame()
 print(" * Dataframe has been loaded from API path: " + NC_REQ_COUNT_DATA_API_PATH)
 
 # Loading the data for the number of new requests
-NEW_REQ_COUNT_DATA_API_PATH = "/reports?field=created_year&filter=created_date>=2016-01-01"
+new_req_count_api_params = {'field':'created_year', 'filter':'created_date>=2016-01-01'}
+NEW_REQ_COUNT_DATA_API_PATH = REPORT_API_PATH_ROOT + urllib.parse.urlencode(new_req_count_api_params)
 print(" * Downloading data from API path: " + NEW_REQ_COUNT_DATA_API_PATH)
 new_req_count_df = pd.read_json(API_HOST + NEW_REQ_COUNT_DATA_API_PATH)
 new_req_count_df = new_req_count_df.groupby(['created_year'])['counts'].sum().to_frame()
 print(" * Dataframe has been loaded from API path: " + NEW_REQ_COUNT_DATA_API_PATH)
 
 # Loading the count of each request types overall
-REQ_COUNT_DATA_API_PATH = "/reports?field=type_name&filter=created_date>=2016-01-01"
+req_count_api_params = {'field':'type_name', 'filter':'created_date>=2016-01-01'}
+REQ_COUNT_DATA_API_PATH = REPORT_API_PATH_ROOT + urllib.parse.urlencode(req_count_api_params)
 print(" * Downloading data from API path: " + REQ_COUNT_DATA_API_PATH)
 req_count_df = pd.read_json(API_HOST + REQ_COUNT_DATA_API_PATH)
 req_count = req_count_df.groupby(['type_name'], as_index=False)['counts'].sum()
 print(" * Dataframe has been loaded from API path: " + REQ_COUNT_DATA_API_PATH)
 
 # Loading the total number of request source
-REQ_SOURCE_COUNT_DATA_API_PATH = "/reports?field=source_name&filter=created_date>=2016-01-01"
+req_source_count_api_params = {'field':'source_name', 'filter':'created_date>=2016-01-01'}
+REQ_SOURCE_COUNT_DATA_API_PATH = REPORT_API_PATH_ROOT + urllib.parse.urlencode(req_source_count_api_params)
 print(" * Downloading data from API path: " + REQ_SOURCE_COUNT_DATA_API_PATH)
 req_source_count_df = pd.read_json(API_HOST + REQ_SOURCE_COUNT_DATA_API_PATH)
 req_source_count = req_source_count_df.groupby(['source_name'])['counts'].sum()
 print(" * Dataframe has been loaded from API path: " + REQ_SOURCE_COUNT_DATA_API_PATH)
 
 # Loading the number of Request Agencies
-REQ_AGENCY_COUNT_DATA_API_PATH = "/reports?field=agency_name&filter=created_date>=2016-01-01"
+req_agency_count_api_params = {'field':'agency_name', 'filter':'created_date>=2016-01-01'}
+REQ_AGENCY_COUNT_DATA_API_PATH = REPORT_API_PATH_ROOT + urllib.parse.urlencode(req_agency_count_api_params)
 print(" * Downloading data from API path: " + REQ_AGENCY_COUNT_DATA_API_PATH)
 req_agency_count_df = pd.read_json(API_HOST + REQ_AGENCY_COUNT_DATA_API_PATH)
 agency_count = req_agency_count_df.groupby(['agency_name'])['counts'].sum()
@@ -82,8 +88,11 @@ req_source_bar_chart = px.bar(
 )
 
 # Median Request Days to Close Box Plot
-stats_df = pd.read_json(API_HOST + '/types/stats')
+REQ_TYPE_STATS_API_PATH  = '/types/stats'
+print(" * Downloading data from API path: " + REQ_TYPE_STATS_API_PATH)
+stats_df = pd.read_json(API_HOST + REQ_TYPE_STATS_API_PATH)
 stats_df = stats_df.sort_values('median', ascending=False)
+print(" * Dataframe has been loaded from API path: " + REQ_TYPE_STATS_API_PATH)
 
 med_days_to_close_box_plot = go.Figure()
 med_days_to_close_box_plot.add_trace(
@@ -106,7 +115,8 @@ med_days_to_close_box_plot.update_layout(
 # Day of Week Bar Chart:
 start_date = datetime.date.today() - datetime.timedelta(days=30)
 end_date = datetime.date.today() - datetime.timedelta(days=1)
-DATE_RANGE_REQ_DATA_API_PATH = f"/reports?filter=created_date>={start_date}&filter=created_date<={end_date}"  # noqa
+date_range_req_data_params = {'filter':f"created_date>={start_date}", 'filter':f"created_date<={end_date}"}
+DATE_RANGE_REQ_DATA_API_PATH = REPORT_API_PATH_ROOT + + urllib.parse.urlencode(date_range_req_data_params)
 print(" * Downloading data for dataframe from API path: " + DATE_RANGE_REQ_DATA_API_PATH)
 date_range_req_df = pd.read_json(API_HOST + DATE_RANGE_REQ_DATA_API_PATH)
 print(" * Dataframe has been loaded from API path: " + DATE_RANGE_REQ_DATA_API_PATH)
