72300d3ea1a2801ed23644f8b1ad8cad991152e8
funbunch <shannan_bunch@hotmail.com>
2022-07-27 16:33:25 -0700

    started adding content to about.jsx. trying to add images now.

--- client/components/main/About.jsx
+++ client/components/main/About.jsx
@@ -1,22 +1,10 @@
 import React from 'react';
-import ReactMarkdown from 'react-markdown';
 import {
   makeStyles,
   Container,
   Grid,
 } from '@material-ui/core';
-import useContentful from '../../hooks/useContentful';
-
-const query = `
-  query {
-    simplePageCollection(where: {slug: "about"}) {
-      items {
-        title
-        body
-      }
-    }
-  }
-`;
+import Image from 'mui-image';
 
 const useStyles = makeStyles({
   root: {
@@ -27,6 +15,9 @@ const useStyles = makeStyles({
     padding: '2em',
     '& h1': {
       fontSize: '2.5em',
+    },
+    '& h2': {
+      fontSize: '2.25em',
     },
     '& img': {
       maxWidth: '100%',
@@ -39,26 +30,58 @@ const useStyles = makeStyles({
 });
 
 const About = () => {
-  const { data, errors } = useContentful(query);
   const classes = useStyles();
 
-  React.useEffect(() => {
-    if (errors) console.log(errors);
-  }, [errors]);
+  // React.useEffect(() => {
+  //   if (errors) console.log(errors);
+  // }, [errors]);
 
   return (
     <>
-      { data
-        && (
-          <Container className={classes.root} maxWidth="md">
-            <Grid container>
-              <Grid item xs={9} md={6}>
-                <h1>{data.simplePageCollection.items[0].title}</h1>
-                <ReactMarkdown>{data.simplePageCollection.items[0].body}</ReactMarkdown>
-              </Grid>
-            </Grid>
-          </Container>
-        )}
+      <Container disableGutters component="main" className={classes.root} maxWidth="md">
+        <Grid container>
+          <Grid item xs={6} md={12}>
+            <h1 align="center">
+              About
+              <span style={{ color: '#FFB100' }}>311</span>
+              <span style={{ color: '#87C8BC' }}>DATA</span>
+            </h1>
+            <p>
+              Each day, Los Angelenos report thousands of 311
+              requests all across LA to resolve issues such as
+              illegal dumping and homeless encampments in their
+              neighborhoods. These requests are then received by
+              relevant agencies, such as the Police, Building and
+              Safety, or Department of Transportation. The agency
+              responds to the request, addresses it, and then closes
+              it once it is fixed.  The expansive amount of data
+              associated with these 311 requests is available online.
+              However, it is difficult to make actionable at the neighborhood
+              level. Thanks to the mayor&apos;s Open Data Initiative,
+              the expansive amount of data associated with these 311
+              requests is available online. The mayor has encouraged
+              us to create apps with this data, and that&apos;s where
+              this project comes in.
+            </p>
+            <h2 style={{ color: '#1D6996', textAlign: 'center' }}>Partners</h2>
+            <p>
+              To empower local residents and Neighborhood Councils to
+              make informed decisions about how to improve their communities
+              using an easy-to-use application, EmpowerLA partnered with Hack
+              For LA  to create the 311 Data project. The 311 Data project makes
+              navigating the wealth of 311 data easier using an open source
+              application built and maintained by volunteers throughout our community.
+            </p>
+          </Grid>
+        </Grid>
+      </Container>
+      <Container maxWidth="md" component="main">
+        <Grid container spacing={5} alignItems="flex-end">
+          <Grid item xs={6} sm={3}>
+            <Image src="../Partners_icon_1.png" duration={0} />
+          </Grid>
+        </Grid>
+      </Container>
     </>
   );
 };
