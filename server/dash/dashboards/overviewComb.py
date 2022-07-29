4cb5e710b46b3cad53531bea4cd35ad830735ed0
Yi Heng Joshua Wu <joshuayhwu@gmail.com>
2022-07-28 20:05:24 -0700

    Update comments

--- server/dash/dashboards/overviewComb.py
+++ server/dash/dashboards/overviewComb.py
@@ -13,7 +13,7 @@ TITLE = "OVERVIEW COMBINED DASHBOARD"
 REPORT_API_PATH_ROOT = "/reports?"
 # DATA
 
-# Loading the dataframe for the NCs and correspoding requests
+# Loading the dataframe for the NCs and corresponding requests.
 nc_req_count_api_params = {'field':'council_name', 'filter':'created_date>=2016-01-01'}
 NC_REQ_COUNT_DATA_API_PATH = REPORT_API_PATH_ROOT + urllib.parse.urlencode(nc_req_count_api_params)
 print(" * Downloading data from API path: " + NC_REQ_COUNT_DATA_API_PATH)
@@ -21,7 +21,7 @@ nc_req_count_df = pd.read_json(API_HOST + NC_REQ_COUNT_DATA_API_PATH)
 nc_req_count_df = nc_req_count_df.groupby(['council_name'])['counts'].sum().sort_values().to_frame()
 print(" * Dataframe has been loaded from API path: " + NC_REQ_COUNT_DATA_API_PATH)
 
-# Loading the data for the number of new requests
+# Loading the data for the number of new requests.
 new_req_count_api_params = {'field':'created_year', 'filter':'created_date>=2016-01-01'}
 NEW_REQ_COUNT_DATA_API_PATH = REPORT_API_PATH_ROOT + urllib.parse.urlencode(new_req_count_api_params)
 print(" * Downloading data from API path: " + NEW_REQ_COUNT_DATA_API_PATH)
@@ -29,7 +29,7 @@ new_req_count_df = pd.read_json(API_HOST + NEW_REQ_COUNT_DATA_API_PATH)
 new_req_count_df = new_req_count_df.groupby(['created_year'])['counts'].sum().to_frame()
 print(" * Dataframe has been loaded from API path: " + NEW_REQ_COUNT_DATA_API_PATH)
 
-# Loading the count of each request types overall
+# Loading the count of each request types overall.
 req_count_api_params = {'field':'type_name', 'filter':'created_date>=2016-01-01'}
 REQ_COUNT_DATA_API_PATH = REPORT_API_PATH_ROOT + urllib.parse.urlencode(req_count_api_params)
 print(" * Downloading data from API path: " + REQ_COUNT_DATA_API_PATH)
@@ -37,7 +37,7 @@ req_count_df = pd.read_json(API_HOST + REQ_COUNT_DATA_API_PATH)
 req_count = req_count_df.groupby(['type_name'], as_index=False)['counts'].sum()
 print(" * Dataframe has been loaded from API path: " + REQ_COUNT_DATA_API_PATH)
 
-# Loading the total number of request source
+# Loading the total number of request source.
 req_source_count_api_params = {'field':'source_name', 'filter':'created_date>=2016-01-01'}
 REQ_SOURCE_COUNT_DATA_API_PATH = REPORT_API_PATH_ROOT + urllib.parse.urlencode(req_source_count_api_params)
 print(" * Downloading data from API path: " + REQ_SOURCE_COUNT_DATA_API_PATH)
@@ -45,7 +45,7 @@ req_source_count_df = pd.read_json(API_HOST + REQ_SOURCE_COUNT_DATA_API_PATH)
 req_source_count = req_source_count_df.groupby(['source_name'])['counts'].sum()
 print(" * Dataframe has been loaded from API path: " + REQ_SOURCE_COUNT_DATA_API_PATH)
 
-# Loading the number of Request Agencies
+# Loading the number of Request Agencies.
 req_agency_count_api_params = {'field':'agency_name', 'filter':'created_date>=2016-01-01'}
 REQ_AGENCY_COUNT_DATA_API_PATH = REPORT_API_PATH_ROOT + urllib.parse.urlencode(req_agency_count_api_params)
 print(" * Downloading data from API path: " + REQ_AGENCY_COUNT_DATA_API_PATH)
@@ -53,7 +53,7 @@ req_agency_count_df = pd.read_json(API_HOST + REQ_AGENCY_COUNT_DATA_API_PATH)
 agency_count = req_agency_count_df.groupby(['agency_name'])['counts'].sum()
 print(" * Dataframe has been loaded from API path: " + REQ_AGENCY_COUNT_DATA_API_PATH)
 
-# Request Share by Agency Pie Chart
+# Request Share by Agency Pie Chart.
 req_agency_count_df = agency_count.to_frame()
 req_agency_count_df.sort_values('counts', ascending=False, inplace=True)
 req_agency_count_df.loc['Others'] = req_agency_count_df[4:].sum()
@@ -72,7 +72,7 @@ req_share_by_agency_pie_chart = px.pie(
 )
 req_share_by_agency_pie_chart.update_layout(margin=dict(l=100, r=100, b=100, t=100))
 
-# Request Type by Source Bar Chart
+# Request Type by Source Bar Chart.
 req_source_count_df = req_source_count.to_frame()
 req_source_count_df.sort_values('counts', ascending=False, inplace=True)
 req_source_count_df.loc['Others'] = req_source_count_df[4:].sum()
@@ -87,7 +87,7 @@ req_source_bar_chart = px.bar(
     orientation='h'
 )
 
-# Median Request Days to Close Box Plot
+# Median Request Days to Close Box Plot.
 REQ_TYPE_STATS_API_PATH  = '/types/stats'
 print(" * Downloading data from API path: " + REQ_TYPE_STATS_API_PATH)
 stats_df = pd.read_json(API_HOST + REQ_TYPE_STATS_API_PATH)
@@ -112,7 +112,7 @@ med_days_to_close_box_plot.update_layout(
     title="Total Median Days to Close by Type",
 )
 
-# Day of Week Bar Chart:
+# Day of Week Bar Chart.
 start_date = datetime.date.today() - datetime.timedelta(days=365)
 end_date = datetime.date.today() - datetime.timedelta(days=1)
 date_range_req_data_params = {'filter':f"created_date>={start_date}", 'filter':f"created_date<={end_date}"}
@@ -133,7 +133,7 @@ num_req_by_day_bar_chart = px.bar(
 num_req_by_day_bar_chart.update_xaxes(categoryorder='array', categoryarray=[
     "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"])
 
-# Total Request by NC
+# Total Request by NC.
 req_by_nc_bar_chart = px.bar(
     nc_req_count_df,
     x=nc_req_count_df.index,
@@ -143,7 +143,7 @@ req_by_nc_bar_chart = px.bar(
 )
 req_by_nc_bar_chart.update_layout(font=dict(size=12))
 
-# LAYOUT VARIABLES
+# LAYOUT VARIABLES.
 INDICATOR_CARD_STYLE = {"text-align": 'center',
     "border": "0.5px black solid", 'width': '18vw', 'display': 'inline-block'}
 BORDER_STYLE = {"border": "0.5px black solid"}
@@ -151,10 +151,10 @@ EQUAL_SPACE_BOX_STYLE = {'display': 'flex', "justify-content": "space-between"}
 TWO_CHART_STYLE = {"width": "48vw", "height": "40vh"}
 DASHBOARD_OUTLINE = "The figures below represent the total number of 311 requests made across LA County from 2016-2021. In 2020, we saw an all-time high with more than 1.4 million requests."
 
-# LAYOUT
+# LAYOUT.
 layout = html.Div([
 
-    # Part 1 of dashboard
+    # Dashboard showing summary statistics, median time to close box plot, and request share by agency.
     html.H1(TITLE + " Pt. 1"),
     html.P(DASHBOARD_OUTLINE, style={'font-size': '18px', 'font-style': 'italic'}),
 
@@ -182,7 +182,7 @@ layout = html.Div([
 
     html.Div(html.Br(), style={"height": "2vh"}),
 
-    # Part 2 of dashboard
+    # Bar charts showing number of requests by day, number of requests by source, and number of requests by NCs.
     html.H1(TITLE + " Pt. 2"),
     html.Div([
         html.Div(dcc.Graph(id='num_req_by_day_bar_chart', figure=num_req_by_day_bar_chart, className="half-graph",
