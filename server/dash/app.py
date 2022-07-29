d98a844bab610486a3e6be0c99cd852fc98c7811
Yi Heng Joshua Wu <joshuayhwu@gmail.com>
2022-07-28 20:01:31 -0700

    Adding period at end of comment

--- server/dash/app.py
+++ server/dash/app.py
@@ -1,7 +1,7 @@
 from dash import Dash, dcc, html
 
 external_stylesheets = ['/static/reports.css']
-# Suppress Callback Exceptions due to multi-page Dashboard layout, some callback id may not exist initially
+# Suppress Callback Exceptions due to multi-page Dashboard layout, some callback id may not exist initially.
 app = Dash(__name__, external_stylesheets=external_stylesheets, suppress_callback_exceptions=True)
 
 # set up default layout
