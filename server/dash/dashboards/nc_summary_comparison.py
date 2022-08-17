aabf25318695a5b350193128097f7187e8c82adc
Yi Heng Joshua Wu <joshuayhwu@gmail.com>
2022-08-16 18:26:02 -0700

    Use np bins

--- server/dash/dashboards/nc_summary_comparison.py
+++ server/dash/dashboards/nc_summary_comparison.py
@@ -394,14 +394,7 @@ def filter_bad_quality_data(df, data_quality_switch=True):
 
     # Data Quality switch to remove outliers as defined by Median +- 1.5*IQR.
     if data_quality_switch:
-        q3, q1 = np.percentile(df.loc[:, "timeToClose"].astype(int), [75, 25])
-        iqr = q3 - q1
-
-        # IQR might be 0 when q3 and q1 are the same value due to narrow distribution or lack of data. 
-        if iqr == 0:
-            num_bins = 100
-        else:
-            num_bins = int((2 * iqr) / (df.shape[0]**(1 / 3)))
+        num_bins = len(np.histogram_bin_edges(df.loc[:, "timeToClose"], bins='fd'))-1
 
         # Log Transform, Compute IQR, then exclude outliers.
         df.loc[:, "logTimeToClose"] = np.log(df.loc[:, "timeToClose"])
