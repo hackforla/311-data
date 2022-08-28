85c959d7b7282a1cb7ae6ba1afa36ca20d6be55a
Edwin J <hungrylulu8@gmail.com>
2022-08-27 17:27:53 -0700

    created first passing test in jest

---
+++ client/jest.config.js
@@ -0,0 +1,23 @@
+/** @type {import('jest').Config} */
+const config = {
+  verbose: true,
+  transform: {
+    '^.+\\.[t|j]sx?$': 'babel-jest',
+  },
+  moduleDirectories: [
+    'node_modules',
+  ],
+  moduleNameMapper: {
+    '^@root(.*)$': '<rootDir>/$1',
+    '^@components(.*)$': '<rootDir>/components/$1',
+    '^@reducers(.*)$': '<rootDir>/redux/reducers/$1',
+    '@styles(.*)$': '<rootDir>/styles/$1',
+    '@assets(.*)$': '<rootDir>/assets/$1',
+    '@utils(.*)$': '<rootDir>/utils/$1',
+    '^.+.(styl|less|sass|scss|png|jpg|ttf|woff|woff2|svg)$': 'jest-transform-stub',
+    '^.+(ReactToastify).css$': 'jest-transform-stub',
+  },
+  moduleFileExtensions: ['js', 'jsx', 'json'],
+};
+
+module.exports = config;
