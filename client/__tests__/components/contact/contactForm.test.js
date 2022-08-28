b71f7d5403a50ff0dc57d104dd80da5bb6794d08
Edwin J <hungrylulu8@gmail.com>
2022-08-27 21:24:00 -0700

    updated contactForm test to use actual store instead of a mock store as per recommendation from redux.js.org/usage/writing-tests

--- client/__tests__/components/contact/contactForm.test.js
+++ client/__tests__/components/contact/contactForm.test.js
@@ -1,28 +1,16 @@
 import ContactForm from '@components/contact/ContactForm'
-// import App from '@root/App'
 import { create } from 'react-test-renderer'
 import { Provider } from 'react-redux';
-import configureStore from 'redux-mock-store';
+import store from '@root/redux/store';
 
 describe("My first snapshot test", ()=>{
     test('testing ContactForm', ()=>{
-        const initialState = {
-            ui: {
-                displayFeedbackSuccess: false,
-                error: {
-                    isOpen: false,
-                  }
-            }            
-        }
-        const mockStore = configureStore()
-        let store = mockStore(initialState)
-
         let tree = create(
             <Provider store={store}>
                 <ContactForm />
             </Provider>
         )
-        // let tree = create(<App />)
         expect(tree.toJSON()).toMatchSnapshot();
     })
+
 })
