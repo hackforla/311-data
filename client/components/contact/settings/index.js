13f76a43bc451b02a52e9b0ec4bef05470bb638c
Edwin J <hungrylulu8@gmail.com>
2022-08-30 00:03:57 -0700

    made some changes as per review comments

---
+++ client/components/contact/settings/index.js
@@ -0,0 +1,17 @@
+
+
+const contactSettings = {
+    toast: {
+        dark: {
+            position: 'top-right',
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+          }
+    }
+}
+
+export default contactSettings;
