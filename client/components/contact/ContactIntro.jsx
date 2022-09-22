069f57f5c14b5affec35f2131855ab7786b460ea
Edwin J <hungrylulu8@gmail.com>
2022-09-22 14:35:48 -0700

    made some formatting changes to ContactIntro.jsx

--- client/components/contact/ContactIntro.jsx
+++ client/components/contact/ContactIntro.jsx
@@ -1,22 +1,41 @@
 import React from 'react';
-import {
-  Grid,
-} from '@material-ui/core';
+import Grid from '@material-ui/core/Grid';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import Typography from '@material-ui/core/Typography';
+import layout from '@theme/layout';
 
-const ContactIntro = () => (
-  <Grid container alignItems="center" justify="center" direction="column">
-    <Grid item style={{ width: '45%' }}>
-      <h1 style={{ textAlign: 'center' }}>
-        {'Don\'t See What You Need?'}
-      </h1>
-      <p>
-        We want to build a tool that works for you. We are open to suggestions and
-        feedback and would love the opportunity to get connected. Feel free to input
-        your information in the contact form below and we will be sure to get back to
-        you within 2-3 business days. Thank you!
-      </p>
-    </Grid>
-  </Grid>
-);
+const useStyles = makeStyles(() => ({
+  contactIntroHeading: layout.contentMarginTop,
+  contactIntroBody: {
+    margin: '1em 0',
+  },
+}));
+
+const ContactIntro = () => {
+  const classes = useStyles();
+
+  return (
+    <Container maxWidth="sm">
+      <Grid container alignItems="center" justify="center" direction="column">
+        <Grid item>
+          <div className={classes.contactIntroHeading}>
+            <Typography variant="subtitle1" align="left">
+              {'Don\'t See What You Need?'}
+            </Typography>
+          </div>
+          <div className={classes.contactIntroBody}>
+            <Typography variant="body1">
+              We want to build a tool that works for you. We are open to suggestions and
+              feedback and would love the opportunity to get connected. Feel free to input
+              your information in the contact form below and we will be sure to get back to
+              you within 2-3 business days. Thank you!
+            </Typography>
+          </div>
+        </Grid>
+      </Grid>
+    </Container>
+  );
+};
 
 export default ContactIntro;
