0a92e142dea537600546a716ca10774116d35e6e
Edwin J <hungrylulu8@gmail.com>
2022-09-22 15:58:21 -0700

    layout.js now exports a custom makeStyles hook to keep common spacing and margin properties while enforcing consistency among pages

--- client/components/contact/ContactIntro.jsx
+++ client/components/contact/ContactIntro.jsx
@@ -1,30 +1,22 @@
 import React from 'react';
 import Grid from '@material-ui/core/Grid';
-import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import Typography from '@material-ui/core/Typography';
-import layout from '@theme/layout';
-
-const useStyles = makeStyles(() => ({
-  contactIntroHeading: layout.contentMarginTop,
-  contactIntroBody: {
-    margin: '1em 0',
-  },
-}));
+import sharedLayout from '@theme/layout';
 
 const ContactIntro = () => {
-  const classes = useStyles();
+  const classes = sharedLayout();
 
   return (
     <Container maxWidth="sm">
       <Grid container alignItems="center" justify="center" direction="column">
         <Grid item>
-          <div className={classes.contactIntroHeading}>
+          <div className={classes.contentMarginTop}>
             <Typography variant="subtitle1" align="left">
               {'Don\'t See What You Need?'}
             </Typography>
           </div>
-          <div className={classes.contactIntroBody}>
+          <div className={classes.contentIntroBody}>
             <Typography variant="body1">
               We want to build a tool that works for you. We are open to suggestions and
               feedback and would love the opportunity to get connected. Feel free to input
--- client/theme/layout.js
+++ client/theme/layout.js
@@ -1,5 +1,12 @@
-export default {
+import { makeStyles, createStyles } from '@material-ui/core/styles';
+
+export default makeStyles(theme => createStyles({
+  // Content pages: Vertical margin just below the TextHeading.
   contentMarginTop: {
-    margin: '2.67em 0.67em 0 0',
+    margin: theme.spacing(5, 1, 0, 0),
   },
-};
+  // Content pages: Vertical margin added to separate content body.
+  contentIntroBody: {
+    margin: theme.spacing(1, 0),
+  },
+}));
