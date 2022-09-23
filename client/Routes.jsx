e61378ee81559454bde06513c9d2ecea0b84c94b
Edwin J <hungrylulu8@gmail.com>
2022-09-23 15:56:55 -0700

    removed blog from Header.jsx and Routes.jsx since they no longer appear in the figma design for v2

--- client/Routes.jsx
+++ client/Routes.jsx
@@ -14,7 +14,6 @@ import Reports from '@components/main/Reports';
 import Privacy from '@components/main/Privacy';
 import Faqs from '@components/main/Faqs';
 import About from '@components/main/About';
-import Blog from '@components/main/Blog';
 import Contact from '@components/contact/Contact';
 import ContentBottom from '@components/common/ContentBottom';
 
@@ -40,7 +39,6 @@ export default function Routes() {
             <Route path="/privacy" component={Privacy} />
             <Route path="/faqs" component={Faqs} />
             <Route path="/about" component={About} />
-            <Route path="/blog" component={Blog} />
             <Route path="/contact" component={Contact} />
             <Route path="/">
               <Redirect to="map" />
