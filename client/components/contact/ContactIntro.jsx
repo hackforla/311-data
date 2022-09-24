309ca9105e7a807cd4b91dc68a26203b6c5f2036
Edwin J <hungrylulu8@gmail.com>
2022-09-24 11:41:09 -0700

    initial v2 contact

--- client/components/contact/ContactIntro.jsx
+++ client/components/contact/ContactIntro.jsx
@@ -1,22 +1,30 @@
 import React from 'react';
-import {
-  Grid,
-} from '@material-ui/core';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import sharedLayout from '@theme/layout';
 
-const ContactIntro = () => (
-  <Grid container alignItems="center" justify="center" direction="column">
-    <Grid item style={{ width: '45%' }}>
-      <h1 style={{ textAlign: 'center' }}>
-        {'Don\'t See What You Need?'}
-      </h1>
-      <p>
-        We want to build a tool that works for you. We are open to suggestions and
-        feedback and would love the opportunity to get connected. Feel free to input
-        your information in the contact form below and we will be sure to get back to
-        you within 2-3 business days. Thank you!
-      </p>
+const ContactIntro = () => {
+  const classes = sharedLayout();
+
+  return (
+    <Grid container alignItems="center" justify="center" direction="column">
+      <Grid item>
+        <div align="left">
+          <Typography variant="subtitle1">
+            {'Don\'t See What You Need?'}
+          </Typography>
+        </div>
+        <div className={classes.contentIntroBody}>
+          <Typography variant="body1">
+            We want to build a tool that works for you. We are open to suggestions and
+            feedback and would love the opportunity to get connected. Feel free to input
+            your information in the contact form below and we will be sure to get back to
+            you within 2-3 business days. Thank you!
+          </Typography>
+        </div>
+      </Grid>
     </Grid>
-  </Grid>
-);
+  );
+};
 
 export default ContactIntro;
