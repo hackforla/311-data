65957e6945472b730b3d101d42cea8a5e60d15d9
Yi Heng Joshua Wu <joshuayhwu@gmail.com>
2022-09-30 11:36:18 -0700

    Testing

--- server/dash/index.py
+++ server/dash/index.py
@@ -1,9 +1,11 @@
 from dash import Dash, dcc, html, Output, Input, callback
 from dashboards import nc_summary_comparison, overview_combined
+from flask import Flask
 
 external_stylesheets = ['/static/reports.css']
 # Suppress Callback Exceptions due to multi-page Dashboard layout, some callback id may not exist initially.
-app = Dash(__name__, external_stylesheets=external_stylesheets, suppress_callback_exceptions=True)
+fl_serve = Flask(__name__)
+app = Dash(__name__, external_stylesheets=external_stylesheets, suppress_callback_exceptions=True, server=fl_serve)
 server = app.server
 available_dashboards = ["nc_summary_comparison", "overview_combined"]
 
@@ -26,4 +28,9 @@ def display_page(pathname):
         ], className='dash-links')
 
 if __name__ == '__main__':
-    app.run_server(host='0.0.0.0', port='5500', debug=True)
+    #app.run_server(host='0.0.0.0', port='5500', debug=True)
+    print("running host")
+    app.run_server()
+# else:
+#     print("Not main")
+#     app.run_server(host='0.0.0.0', port='5500', debug=True)
