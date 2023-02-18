09ede1a3e5f7273c40d51864d89cd23e1f74f1f1
Bryan Tran <bryantran1004@gmail.com>
2023-02-17 22:37:29 -0500

    Sample only 1% of api status calls (#1464)
    * Sample only 1% of api status calls
    
    * check if path exist in request context

--- server/api/code/lacity_data_api/asgi.py
+++ server/api/code/lacity_data_api/asgi.py
@@ -10,10 +10,21 @@ from .main import get_app
 
 app = get_app()
 
+sample_rate_for_api_calls = {
+    "/status/api": 0.01,
+}
+
+
+def traces_sampler(ctx):
+    if ctx.get("path") and ctx["path"] in sample_rate_for_api_calls:
+        return sample_rate_for_api_calls[ctx["path"]]
+    return 1.0  # Trace all
+
+
 if SENTRY_URL:
     sentry_sdk.init(
         SENTRY_URL,
-        traces_sample_rate=1.0,
-        integrations=[SqlalchemyIntegration(), RedisIntegration()]
+        traces_sampler=traces_sampler,
+        integrations=[SqlalchemyIntegration(), RedisIntegration()],
     )
     app = SentryAsgiMiddleware(app)
