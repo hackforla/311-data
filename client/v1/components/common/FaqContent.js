b91f793b87e741411e6fe1c5423e89f55189e34b
chencc33 <chencc3333@gmail.com>
2023-02-21 22:00:23 -0700

    update FAQ content

--- client/v1/components/common/FaqContent.js
+++ client/v1/components/common/FaqContent.js
@@ -7,28 +7,28 @@ export const FAQS = [
   {
     question: 'What are Neighborhood Councils?',
     answer: {
-      p1: `LA’s 99 Neighborhood Councils together form the grassroots level of the Los Angeles City government. 
-      The system was created to connect LA’s diverse communities to City Hall, and was established in 1999 by an amendment to the City Charter. 
-      While Neighborhood Council board members are volunteers, they are public officials elected to office by the members of their community.`,
+      p1: `Los Angeles' 99 Neighborhood Councils make up the grassroots level of the City's government.
+      The system was created in 1999 by an amendment to the City Charter with the purpose of connecting LA's diverse communities to City Hall.
+      Board members of these Neighborhood Councils are public officials elected to their positions by the members of their respective communities and serve as volunteers.`,
     },
     possibleSearchTerms: ['What are Neighborhood Councils?', 'Neighborhood Councils', 'When were Neighborhood Councils Created?', 'Who Serves on Neighborhood Councils?'],
   },
   {
     question: 'What do Neighborhood Councils do?',
     answer: {
-      p1: `Since Neighborhood Councils hold their meetings in the communities they serve, they are an important avenue for public participation 
-      in the City of Los Angeles, and give the members of a community the chance to have input on decisions that affect their quality of life, 
-      and the services they receive from the City.`,
+      p1: `Neighborhood Councils play a crucial role in the City of Los Angeles by holding meetings in the communities they serve,
+      providing a valuable platform for public engagement and giving community members a voice
+      in decisions that impact their daily lives and the services provided by the city.`,
     },
     possibleSearchTerms: ['What do Neighborhood Councils do?', 'Purpose of Neighborhood Councils', 'Why do we have Neighborhood Councils?', 'Why are Neighborhood Councils Important?'],
   },
   {
     question: 'How are Neighborhood Councils funded?',
     answer: {
-      p1: `Neighborhood Councils receive public funds of about $42,000 each year to support their activities. Each member takes state-mandated 
-      training on the ethical management of public funds, and the funds must be allocated by board consensus. The funds may be used to create 
-      events and programs that respond to community needs, or spent to advocate for issues that the board cares about such as crime prevention, 
-      better roads and streets, safe spaces for children, help for the homeless, arts, or local economic development`,
+      p1: `Neighborhood Councils are allocated approximately $42,000 annually in public funds to support their operations.
+      The members are required to undergo state-mandated training on the responsible handling of these funds and decisions
+      on their allocation must be made through collective agreement. These funds can be utilized to organize events and programs tailored to community needs,
+      or to advocate for important issues such as crime prevention, infrastructure improvements, safe spaces for children, homeless assistance, arts, and local economic growth.`,
     },
     possibleSearchTerms: ['How are Neighborhood Councils funded?', 'How are NC\'s Funded?', 'Funding', 'NC Funding', 'Neighborhood Council Funds'],
   },
@@ -43,7 +43,7 @@ export const FAQS = [
       p3: 'Then select Data Range, Location(s), and Request Type filter options.',
       img2: [FaqMapSelection, 50],
       h43: 'Step 3',
-      p4: `Results updated on the map on the right side panel. You can toggle between 'Map' or 'Data' modes. 
+      p4: `Results updated on the map on the right side panel. You can toggle between 'Map' or 'Data' modes.
           'Data' mode provides bars & charts of the data. You can also export charts from this view, by clicking on 'Export'.`,
     },
     possibleSearchTerms: ['How Do I Search for 311 Requests in my Neighborhood Council', 'Search for NC', 'Search for Neighborhood Council', 'Search 311 Request', '311 Requests in My Neighborhood Council'],
@@ -62,9 +62,9 @@ export const FAQS = [
       p1: 'Click on "Compare Councils" on top menu bar',
       img1: [FaqCompareCouncils, 30],
       h42: 'Step 2',
-      p2: `Click on 'Chart' on the secondary menu bar on left. Then select Data Range, District, Chart, 
+      p2: `Click on 'Chart' on the secondary menu bar on left. Then select Data Range, District, Chart,
       and Request Type filter options.`,
-      p3: `> On District Selection > Click on 'Add District' > Make your selection (you can type in the dropdown 
+      p3: `> On District Selection > Click on 'Add District' > Make your selection (you can type in the dropdown
       and select from suggestions)`,
       p4: '> Select the Chart Type you need in "Chart Selection" section',
       p5: '> Select the Chart Type you need in "Chart" section',
