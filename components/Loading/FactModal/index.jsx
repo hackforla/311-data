126a7b092f5ff5fa97e2a3fad5bcb270c859de72
Johnny Wu <skydodle@gmail.com>
2024-03-27 12:10:19 -0700

    Style FactModal to below

--- components/Loading/FactModal/index.jsx
+++ components/Loading/FactModal/index.jsx
@@ -1,49 +1,51 @@
 import React, { useState, useEffect } from 'react';
-import makeStyles from '@mui/styles/makeStyles';
-import { Modal, Typography } from '@mui/material';
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Modal from '@mui/material/Modal';
+import Typography from '@mui/material/Typography';
 import { seconds } from '@utils';
 import facts from '@data/facts';
 
-const useStyles = makeStyles(theme => ({
-  modal: {
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  paper: {
-    backgroundColor: theme.palette.background.paper,
-    boxShadow: theme.shadows[5],
-    padding: theme.spacing(2, 4, 3),
-    textAlign: 'center',
-  },
+const StyledModal = styled(Modal)({
+  display: 'flex',
+  alignItems: 'flex-end',
+  justifyContent: 'center',
+});
+
+const StyledBox = styled(Box)(({ theme }) => ({
+  position: 'absolute',
+  bottom: '25vh',
+  backgroundColor: '#424242',
+  padding: theme.spacing(2, 4, 3),
+  textAlign: 'center',
 }));
 
 export default function FactModal() {
-  const classes = useStyles();
   const [open, setOpen] = useState(true);
   const [currentFactIndex, setCurrentFactIndex] = useState(0);
   const factsLength = facts.length;
 
   useEffect(() => {
-    let intervalId = null;
-    intervalId = setInterval(() => {
-      setCurrentFactIndex((currentFactIndex + 1) % factsLength);
+    const intervalId = setInterval(() => {
+      setCurrentFactIndex(prevIndex => (prevIndex + 1) % factsLength);
     }, seconds(5));
     return () => clearInterval(intervalId);
-  }, [currentFactIndex, factsLength]);
+  }, [factsLength]);
 
   const handleClose = () => {
     setOpen(false);
   };
 
   return (
-    <Modal open={open} onClose={handleClose} className={classes.modal}>
-      <div className={classes.paper}>
+    <StyledModal open={open}>
+      <StyledBox>
         <Typography variant="h5" gutterBottom>
-          Did you know?
+          Did you know Fact?
         </Typography>
-        <Typography variant="body1">{facts[currentFactIndex]}</Typography>
-      </div>
-    </Modal>
+        <Typography variant="body1">
+          {facts[currentFactIndex]}
+        </Typography>
+      </StyledBox>
+    </StyledModal>
   );
 }
