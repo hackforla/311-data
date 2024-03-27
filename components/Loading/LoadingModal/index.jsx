f8c7660ba21baaa2fe7fe10dd83d40a782df2119
Johnny Wu <skydodle@gmail.com>
2024-03-27 15:54:18 -0700

    Build LoadingModal: Convert to MUI v5 'styled' from MUI v4 'makeStyle'; Remove click out logic; Update text typography and styling; Import 311 logo

--- components/Loading/LoadingModal/index.jsx
+++ components/Loading/LoadingModal/index.jsx
@@ -1,49 +1,51 @@
-import React, { useState, useEffect } from 'react';
-import makeStyles from '@mui/styles/makeStyles';
-import { Modal, Typography } from '@mui/material';
-import { seconds } from '@utils';
-import facts from '@data/facts';
+import React from 'react';
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Modal from '@mui/material/Modal';
+import Typography from '@mui/material/Typography';
+import fonts from '@theme/fonts';
+import LoadingModal311Logo from '@assets/311Logo.png';
 
-const useStyles = makeStyles(theme => ({
-  modal: {
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  paper: {
-    backgroundColor: theme.palette.background.paper,
-    boxShadow: theme.shadows[5],
-    padding: theme.spacing(2, 4, 3),
-    textAlign: 'center',
-  },
+const StyledModal = styled(Modal)({
+  display: 'flex',
+  alignItems: 'flex',
+  justifyContent: 'center',
+});
+
+const StyledBox = styled(Box)(({ theme }) => ({
+  position: 'absolute',
+  bottom: '35vh',
+  backgroundColor: '#29404F',
+  padding: theme.spacing(4, 4, 4),
+  boxShadow: theme.shadows[5],
+  textAlign: 'center',
+  maxWidth: '533px',
+  maxHeight: '469px',
+  borderRadius: '20px',
 }));
 
+const StyledTypography = styled(Typography)({
+  fontSize: '18px',
+  fontFamily: fonts.family.roboto,
+  fontWeight: fonts.weight.medium,
+});
+
 export default function LoadingModal() {
-  const classes = useStyles();
-  const [open, setOpen] = useState(true);
-  const [currentFactIndex, setCurrentFactIndex] = useState(0);
-  const factsLength = facts.length;
-
-  useEffect(() => {
-    let intervalId = null;
-    intervalId = setInterval(() => {
-      setCurrentFactIndex((currentFactIndex + 1) % factsLength);
-    }, seconds(5));
-    return () => clearInterval(intervalId);
-  }, [currentFactIndex, factsLength]);
-
-  const handleClose = () => {
-    setOpen(false);
-  };
-
   return (
-    <Modal open={open} onClose={handleClose} className={classes.modal}>
-      <div className={classes.paper}>
-        <Typography variant="h5" gutterBottom>
-          Did you know?
-        </Typography>
-        <Typography variant="body1">{facts[currentFactIndex]}</Typography>
-      </div>
-    </Modal>
+    <StyledModal open>
+      <StyledBox>
+        <img src={LoadingModal311Logo} width={300} alt="311 data logo" />
+        <StyledTypography variant="body1">
+          Loading data points and map. Please give us a moment.
+          For official information about 311 services in Los Angeles,
+          please visit MyLA311.
+        </StyledTypography>
+        <StyledTypography variant="body1">
+          Loading data points and map. Please give us a moment.
+          For official information about 311 services in Los Angeles,
+          please visit MyLA311.
+        </StyledTypography>
+      </StyledBox>
+    </StyledModal>
   );
 }
