e090e52a71770f5d33b0f357d820afead2288cb7
Johnny Wu <skydodle@gmail.com>
2024-03-27 12:28:53 -0700

    Replace MUIv4 makeStyle with MUIv5 styled

--- components/Loading/FactModal/index.jsx
+++ components/Loading/FactModal/index.jsx
@@ -1,47 +1,46 @@
 import React, { useState, useEffect } from 'react';
-import makeStyles from '@mui/styles/makeStyles';
-import { Modal, Typography } from '@mui/material';
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Modal from '@mui/material/Modal';
+import Typography from '@mui/material/Typography';
 import { seconds } from '@utils';
 import facts from '@data/facts';
 
-const useStyles = makeStyles(theme => ({
-  modal: {
-    display: 'flex',
-    alignItems: 'flex-end',
-    justifyContent: 'center',
-  },
-  paper: {
-    position: 'absolute',
-    bottom: '25vh',
-    backgroundColor: '#424242',
-    padding: theme.spacing(2, 4, 3),
-    textAlign: 'center',
-  },
+const StyledModal = styled(Modal)({
+  display: 'flex',
+  alignItems: 'flex-end',
+  justifyContent: 'center',
+});
+
+const StyledBox = styled(Box)(({ theme }) => ({
+  position: 'absolute',
+  bottom: '25vh',
+  backgroundColor: '#424242',
+  padding: theme.spacing(2, 4, 3),
+  textAlign: 'center',
 }));
 
 export default function FactModal() {
-  const classes = useStyles();
   const [open, setOpen] = useState(true);
   const [currentFactIndex, setCurrentFactIndex] = useState(0);
   const factsLength = facts.length;
 
   useEffect(() => {
-    let intervalId = null;
-    intervalId = setInterval(() => {
-      setCurrentFactIndex((currentFactIndex + 1) % factsLength);
+    const intervalId = setInterval(() => {
+      setCurrentFactIndex(prevIndex => (prevIndex + 1) % factsLength);
     }, seconds(5));
     return () => clearInterval(intervalId);
-  }, [currentFactIndex, factsLength]);
+  }, [factsLength]);
 
   return (
-    <Modal open={open} className={classes.modal}>
-      <div className={classes.paper}>
+    <StyledModal open={open}>
+      <StyledBox>
         <Typography variant="body1">
           Did you know?
           {' '}
           {facts[currentFactIndex]}
         </Typography>
-      </div>
-    </Modal>
+      </StyledBox>
+    </StyledModal>
   );
 }
