b05ff1e814207844bcb99da79d0a0724bfaf27d7
Johnny Wu <skydodle@gmail.com>
2024-03-27 18:43:23 -0700

    Add official 311 gov link

--- components/Loading/LoadingModal/index.jsx
+++ components/Loading/LoadingModal/index.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
 import { styled } from '@mui/material/styles';
-import Box from '@mui/material/Box';
-import Modal from '@mui/material/Modal';
-import Typography from '@mui/material/Typography';
+import { Box, Modal, Typography, Link } from '@mui/material';
 import fonts from '@theme/fonts';
 import LoadingModal311Logo from '@assets/311Logo.png';
+import colors from '@theme/colors';
 
 const StyledModal = styled(Modal)({
   display: 'flex',
@@ -14,7 +13,7 @@ const StyledModal = styled(Modal)({
 
 const StyledBox = styled(Box)(({ theme }) => ({
   position: 'absolute',
-  bottom: '35vh',
+  bottom: '40vh',
   backgroundColor: '#29404F',
   padding: theme.spacing(4, 4, 4),
   boxShadow: theme.shadows[5],
@@ -30,21 +29,30 @@ const StyledTypography = styled(Typography)({
   fontWeight: fonts.weight.medium,
 });
 
+const ExternalLink = styled(Link)({
+  color: colors.primaryFocus,
+  textDecoration: 'none',
+  '&:hover': {
+    textDecoration: 'none',
+  },
+});
+
 export default function LoadingModal() {
   return (
-    <StyledModal open>
+    <StyledModal open disableAutoFocus disableEnforceFocus disableRestoreFocus keepMounted>
       <StyledBox>
         <img src={LoadingModal311Logo} width={300} alt="311 data logo" />
         <StyledTypography variant="body1">
           Loading data points and map. Please give us a moment.
           For official information about 311 services in Los Angeles,
-          please visit MyLA311.
+          please visit
+          {' '}
+          <ExternalLink href="https://lacity.gov/myla311" target="_blank" rel="noopener noreferrer" aria-label="Visit MyLA311 for official information (opens in a new tab)">
+            MyLA311
+          </ExternalLink>
+          .
         </StyledTypography>
-        <StyledTypography variant="body1">
-          Loading data points and map. Please give us a moment.
-          For official information about 311 services in Los Angeles,
-          please visit MyLA311.
-        </StyledTypography>
+
       </StyledBox>
     </StyledModal>
   );
