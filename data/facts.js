a92aa405c700f1015fe168254168bd3560377265
Drake Nguyen <drakeknguyen@yahoo.com>
2024-06-05 18:49:52 -0700

    modify fun fact list to approved fact list

--- data/facts.js
+++ data/facts.js
@@ -3,15 +3,15 @@ import { shuffle } from '@utils';
 /* Source: https://medium.com/datala/latest */
 const facts = shuffle([
   'The LA Neighborhood Council system comprises 99 Neighborhood Councils, each serving approximately 40,000 people.',
-  'In some cases, the boundaries of two or more Certified Neighborhood Councils may overlap if the proposed area is designed for public use, such as a park, school, library, police or fire station, or major thoroughfare; or houses a historical landmark or facility.', 
+  'In some cases, the boundaries of two or more Certified Neighborhood Councils may overlap if the proposed area is designed for public use, such as a park, school, library, police or fire station, or major thoroughfare; or houses a historical landmark or facility.',
   'The Neighborhood Council system was launched in 1999 to ensure that the City government stays connected and adaptive to the unique needs and diverse characteristics of LA’s various communities, promoting more inclusive and representative local governance.',
-  'Hack for LA is a volunteer-driven civic tech organization and is a project of Civic Tech Structure, Inc.—an organization that empowers nonprofits and governments through innovative solutions, scaling, and experimentation, to drive positive change and community impact.', 
-  'Spanning 4,084 square miles, LA County is one of the largest in the United States.', 
-  'With nearly 10 million residents—accounting for 27% of California’s total population—LA County is the most populous of any county in the United States.', 
+  'Hack for LA is a volunteer-driven civic tech organization and is a project of Civic Tech Structure, Inc.—an organization that empowers nonprofits and governments through innovative solutions, scaling, and experimentation, to drive positive change and community impact.',
+  'Spanning 4,084 square miles, LA County is one of the largest in the United States.',
+  'With nearly 10 million residents—accounting for 27% of California’s total population—LA County is the most populous of any county in the United States.',
   'LA County encompasses 88 cities and over 120 unincorporated communities.',
   'Neighborhood Councils are funded by taxpayer dollars, and their board members, who are elected by local communities, serve as volunteer LA City government officials.',
   'Each Neighborhood Council is distinct, with its own board structure and stakeholder representation. Board sizes range from 7 to 35 members, with terms typically lasting two years.',
   'Neighborhood Councils receive $32,000 annually in public funds. These funds support community events, programs, and advocacy initiatives, addressing issues such as infrastructure, public safety, and economic development.',
-  ]);
-  
-  export default facts;
+]);
+
+export default facts;
