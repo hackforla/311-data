18e57fd29329ca8a36bcc8831a728444e953709f
Andrew Aquino <dawneraq@gmail.com>
2024-06-20 19:30:13 -0700

    feat: add import aliases to Vite config

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig(() => {
   return {
@@ -7,5 +8,21 @@ export default defineConfig(() => {
       outDir: 'dist',
     },
     plugins: [react()],
+    resolve: {
+      alias: {
+        '@root': __dirname,
+        '@data': path.resolve(__dirname, 'data'),
+        '@theme': path.resolve(__dirname, 'theme'),
+        '@components': path.resolve(__dirname, 'components'),
+        '@dashboards': path.resolve(__dirname, 'components/Dashboards'),
+        '@hooks': path.resolve(__dirname, 'hooks'),
+        '@reducers': path.resolve(__dirname, 'redux/reducers'),
+        '@styles': path.resolve(__dirname, 'styles'),
+        '@assets': path.resolve(__dirname, 'assets'),
+        '@utils': path.resolve(__dirname, 'utils'),
+        '@settings': path.resolve(__dirname, 'settings'),
+        '@db': path.resolve(__dirname, 'components/db'),
+      },
+    },
   };
 });
