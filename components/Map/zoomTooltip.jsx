df9c41d899a182acccf4dcd4bc5a53e47e771e12
Danielle <danielle@kendallspace.net>
2024-10-26 15:03:09 -0700

    update to match MUI docs for custom styled tooltip

--- components/Map/zoomTooltip.jsx
+++ components/Map/zoomTooltip.jsx
@@ -1,107 +1,51 @@
-import React, { useState } from 'react';
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
-import makeStyles from '@mui/styles/makeStyles';
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+// import Button from '@mui/material/Button';
+import Tooltip, { tooltipClasses } from '@mui/material/Tooltip';
+import Typography from '@mui/material/Typography';
 
-import ToggleButton from '@mui/material/ToggleButton';
-import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
-import { updateRequestStatus } from '@reducers/filters';
-import ArrowToolTip from '@components/common/ArrowToolTip';
-
-const useStyles = makeStyles(() => ({
-  header: {
-    marginBottom: 5,
-    fontFamily: 'Roboto',
-  },
-  root: {
-    display: 'flex',
-    justifyContent: 'center',
-  },
-  button: {
-    width: '50%',
-  },
-  tooltipParagraph: {
-    margin: '1px',
-  },
-}));
-
-function StatusSelector({ updateStatusFilter, requestStatus }) {
-  const classes = useStyles();
-  const [selection, setSelection] = useState('open');
-
-  const handleSelection = (event, newSelection) => {
-    setSelection(newSelection);
+export default function ZoomTooltip() {
+  const [open, setOpen] = React.useState(false);
+  const handleClose = () => {
+    setOpen(false);
   };
-
+  const handleOpen = () => {
+    setOpen(true);
+  };
+  const HtmlTooltip = styled(({ className, ...props }) => (
+    // eslint-disable-next-line react/jsx-props-no-spreading
+    <Tooltip {...props} classes={{ popper: className }} />
+  ))(({ theme }) => ({
+    [`& .${tooltipClasses.tooltip}`]: {
+      backgroundColor: '#29404f',
+      color: 'white',
+      maxWidth: 300,
+      fontSize: theme.typography.pxToRem(10),
+      border: '1px solid #29404f',
+    },
+  }));
   return (
-    <>
-      <ArrowToolTip iconStyle={classes.iconStyle}>
-        <p className={classes.tooltipParagraph}>
-          <strong>
-            Zoom features are limited while locked into a
-            neighborhood council.
-          </strong>
-          {' '}
-          <br />
-          To reset zoom features, please exit by clicking out of
-          the selected area.
-        </p>
-      </ArrowToolTip>
-      <ToggleButtonGroup
-        value={selection}
-        onChange={handleSelection}
-        exclusive
-        className={classes.root}
-      >
-        <ToggleButton
-          className={classes.button}
-          selected={requestStatus.open && !requestStatus.closed}
-          onClick={() => updateStatusFilter('open')}
-          value="open"
-          disableRipple
-        >
-          Open
-        </ToggleButton>
-        <ToggleButton
-          className={classes.button}
-          selected={requestStatus.open && requestStatus.closed}
-          onClick={() => updateStatusFilter('all')}
-          value="all"
-          disableRipple
-        >
-          All
-        </ToggleButton>
-        <ToggleButton
-          className={classes.button}
-          selected={requestStatus.closed && !requestStatus.open}
-          onClick={() => updateStatusFilter('closed')}
-          value="closed"
-          disableRipple
-        >
-          Closed
-        </ToggleButton>
-      </ToggleButtonGroup>
-    </>
+    <HtmlTooltip
+      open={open}
+      onClose={handleClose}
+      onOpen={handleOpen}
+      placement="top-end"
+      arrow
+      title={(
+        <div>
+          <Typography color="inherit">
+            <strong>
+              Zoom features are limited while locked into a
+              neighborhood council.
+            </strong>
+            <br />
+            To reset zoom features, please exit by clicking out of the
+            selected area.
+          </Typography>
+        </div>
+      )}
+    >
+      {/* <Button>-</Button> */}
+    </HtmlTooltip>
   );
 }
-
-const mapStateToProps = state => ({
-  requestStatus: state.filters.requestStatus,
-});
-
-const mapDispatchToProps = dispatch => ({
-  updateStatusFilter: status => dispatch(updateRequestStatus(status)),
-});
-
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps,
-)(StatusSelector);
-
-StatusSelector.propTypes = {
-  updateStatusFilter: PropTypes.func.isRequired,
-  requestStatus: PropTypes.shape({
-    open: PropTypes.bool.isRequired,
-    closed: PropTypes.bool.isRequired,
-  }).isRequired,
-};
